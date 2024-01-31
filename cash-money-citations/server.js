92bd3a0eacc72de91cd888a1e0fee23fe414053f
chrgarcia19 <christian.garcia@valpo.edu>
2024-01-31 16:14:02 -0600

    Added the ability to run the server without dev privileges

---
+++ cash-money-citations/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+    const next = require('next');
+
+    const dev = process.env.NODE_ENV !== 'production';
+    const app = next({ dev });
+    const handle = app.getRequestHandler();
+
+    app.prepare().then(() => {
+    http.createServer((req, res) => {
+        handle(req, res);
+    }).listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000');
+    });
+});

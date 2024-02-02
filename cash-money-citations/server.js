f51fb63ff25bde6ca8c254cad2cae8efe4b49632
xyclose11 <harrisgus1@hotmail.com>
2024-02-02 15:41:39 -0600

    converted server.js to HTTPS, added cert and key files

--- cash-money-citations/server.js
+++ cash-money-citations/server.js
@@ -1,15 +1,22 @@
-const http = require('http');
-    const next = require('next');
+const https = require('https');
+const fs = require('fs');
+const next = require('next');
 
-    const dev = process.env.NODE_ENV !== 'production';
-    const app = next({ dev });
-    const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-    app.prepare().then(() => {
-    http.createServer((req, res) => {
+
+const options = {
+    key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert")
+}
+
+app.prepare().then(() => {
+    https.createServer(options, (req, res) => {
         handle(req, res);
-    }).listen(3000, (err) => {
-        if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
-    });
+      })
+      .listen(443);
 });
+
+

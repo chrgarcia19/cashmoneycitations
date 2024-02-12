aa94d5f24ab5ec35dea5e935347046f428e7eb41
thqmos <thomashohnholt@gmail.com>
2024-02-12 13:53:24 -0600

    Navbar and crud form styling

--- cash-money-citations/src/components/ContributorForm.tsx
+++ cash-money-citations/src/components/ContributorForm.tsx
@@ -54,38 +54,44 @@ const ContributorForm: React.FC<ContributorFormProps> = ({ updateFormData, contr
 
   return (
     <div className="App">
+      <div className="flex justify-center items-center">
+        <h1 className="text-xl align-middle">Contributor Information (Type, First Name, Last Name, Middle Initial)</h1>
+      </div>
       <form>
         {formFields.map((form, index) => (
-          <div key={index}>
+          <div key={index} className="display-block inline-flex">
             <input
               name='contributorType'
               placeholder='Contributor Type (Author, Editor, etc.)'
               onChange={(event) => handleFormChange(event, index)}
               value={form.contributorType}
+              className="w-1/3 pr-0.5 mr-0.5"
             />
             <input
               name='contributorFirstName'
               placeholder='Contributor First Name'
               onChange={(event) => handleFormChange(event, index)}
               value={form.contributorFirstName}
+              className="w-1/3 pr-0.5 mr-0.5"
             />
             <input
               name='contributorLastName'
               placeholder='Contributor Last Name'
               onChange={(event) => handleFormChange(event, index)}
               value={form.contributorLastName}
+              className="w-1/3 pr-0.5 mr-0.5"
             />
             <input
               name='contributorMiddleI'
               placeholder='Contributor Middle Initial'
               onChange={(event) => handleFormChange(event, index)}
               value={form.contributorMiddleI}
+              className="w-2/12 pr-0.5 mr-0.5"
             />
-            <button type="button" onClick={() => removeFields(index)}>Remove</button>
+            <button type="button" className="m-0 text-white bg-red-500 hover:bg-red-900 rounded-lg text-sm" onClick={() => removeFields(index)}>Remove</button>
           </div>
         ))}
-        <button type="button" onClick={addFields}>Add More..</button>
-        <br />
+        <button type="button" className="text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg" onClick={addFields}>Add More..</button>
       </form>
     </div>
   );

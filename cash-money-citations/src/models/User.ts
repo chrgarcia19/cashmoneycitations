ce12259fdcd3f6d7f43a0d016c487444107fc74b
thqmos <thomashohnholt@gmail.com>
2024-02-12 10:31:58 -0600

    Resolved issue where backend validations caused registration to not go through

--- cash-money-citations/src/models/User.ts
+++ cash-money-citations/src/models/User.ts
@@ -13,35 +13,26 @@ const UserSchema = new mongoose.Schema<Users>({
     username: {
         /*The username associated with the account*/
         type: String,
-        required: [true, "Please provide a username for the user."],
     },
     first_name: {
         /* The name of the user */
 
         type: String,
-        required: [true, "Please provide a first name for the user."],
-        maxlength: [30, "First name cannot be more than 30 characters"],
     },
     last_name: {
         /* The name of the user */
 
         type: String,
-        required: [true, "Please provide a Last name for the user."],
-        maxlength: [30, "Last name cannot be more than 30 characters"],
     },
     email: {
         /* The email of the user */
 
         type: String,
-        required: [true, "Please provide the user's email"],
-        maxlength: [60, "Email cannot be more than 60 characters"],
     },
     password: {
         /* The password of the user */
 
         type: String,
-        required: [true, "Please provide the user's password"],
-        minlength: [8, "Password must be at least 8 characters"],
     },
 });
 

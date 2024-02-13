ad2d504cc56fe7d0576733db75592176f8a1f630
chrgarcia19 <christian.garcia@valpo.edu>
2024-02-13 17:43:11 -0600

    Changed sign up to use next-auth

--- cash-money-citations/src/app/api/auth/[...nextauth]/route.ts
+++ cash-money-citations/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,45 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcryptjs';
 import { authOptions } from "@/app/lib/auth";
+import User from "@/models/User";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler = NextAuth({
+    session: {
+      strategy: 'jwt',
+    },
+    pages: {
+      signIn: '/login',
+    },
+    providers: [
+      CredentialsProvider({
+        credentials: {
+          username: {},
+          password: {},
+        },
+        async authorize(credentials, req) {
+          const response = await User.findById(credentials?.username);
+          const user = response.rows[0];
+  
+          const passwordCorrect = await compare(
+            credentials?.password || '',
+            user.password
+          );
+  
+          console.log({ passwordCorrect });
+  
+          if (passwordCorrect) {
+            return {
+              id: user.id,
+              username: user.username,
+              email: user.email,
+            };
+          }
+  
+          return null;
+        },
+      }),
+    ],
+  });
+  
+  export { handler as GET, handler as POST };
---
+++ cash-money-citations/src/app/api/auth/register/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import dbConnect from "../../../../utils/dbConnect";
+import User from "../../../../models/User";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    try {
+        
+        const req = await request.json();
+
+        const { username, firstName, lastName, email, password } = req;
+
+        const userExists = await User.findOne({email});
+
+        if(userExists){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        // Create a new user
+        const user = new User({
+            username,
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        });
+
+        // Save the user to the database
+        await user.save();
+        return NextResponse.json({ success: true, data: user, message: "User created"}, {status: 201});
+    } catch (error) {
+        console.log({ error });
+        return NextResponse.json({ success: false }, { status: 400 });
+    }
+  
+}

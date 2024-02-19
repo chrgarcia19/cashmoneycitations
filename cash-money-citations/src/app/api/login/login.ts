81a9c37cbaeda39dca52e322017da202aff207c1
xyclose11 <harrisgus1@hotmail.com>
2024-02-19 12:12:58 -0600

    Commented out all unnecessary code to simplify authenticated

--- cash-money-citations/src/app/api/login/login.ts
+++ cash-money-citations/src/app/api/login/login.ts
@@ -1,42 +1,42 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import dbConnect from "../../../utils/dbConnect";
-import User from "../../../models/User";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import bcrypt from 'bcryptjs';
+// import jwt from 'jsonwebtoken';
+// import dbConnect from "../../../utils/dbConnect";
+// import User from "../../../models/User";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const { method } = req;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ) {
+//   const { method } = req;
 
-  await dbConnect();
+//   await dbConnect();
 
-  switch (method) {
-    case "POST":
-      try {
-        const { username, password } = req.body;
+//   switch (method) {
+//     case "POST":
+//       try {
+//         const { username, password } = req.body;
 
-        // Find the user in the database
-        const user = await User.findOne({ username });
+//         // Find the user in the database
+//         const user = await User.findOne({ username });
 
-        // If the user doesn't exist or the password is incorrect, send an error
-        if (!user || !await bcrypt.compare(password, user.password)) {
-          return res.status(400).json({ message: 'Invalid username or password' });
-        }
+//         // If the user doesn't exist or the password is incorrect, send an error
+//         if (!user || !await bcrypt.compare(password, user.password)) {
+//           return res.status(400).json({ message: 'Invalid username or password' });
+//         }
 
-        // Create a JWT
-        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret');
+//         // Create a JWT
+//         const token = jwt.sign({ userId: user._id }, 'your_jwt_secret');
 
-        // Send the JWT to the client
-        res.status(200).json({ token });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
-}
+//         // Send the JWT to the client
+//         res.status(200).json({ token });
+//       } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: 'Internal server error' });
+//       }
+//       break;
+//     default:
+//       res.status(400).json({ success: false });
+//       break;
+//   }
+// }
--- cash-money-citations/src/app/api/login/route.ts
+++ cash-money-citations/src/app/api/login/route.ts
@@ -1,28 +1,28 @@
-import { NextResponse } from "next/server";
-import dbConnect from "../../../utils/dbConnect";
-import User from "../../../models/User";
+// import { NextResponse } from "next/server";
+// import dbConnect from "../../../utils/dbConnect";
+// import User from "../../../models/User";
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
-export async function GET() {
+// export async function GET() {
 
-  await dbConnect();
+//   await dbConnect();
 
-  try {
-    const users = await User.find({}); /* find all the data in our database */
-    return NextResponse.json({ success: true, data: users }, { status: 200});
-  } catch (error) {
-    return NextResponse.json({ success: false }, { status: 400});
-  }
+//   try {
+//     const users = await User.find({}); /* find all the data in our database */
+//     return NextResponse.json({ success: true, data: users }, { status: 200});
+//   } catch (error) {
+//     return NextResponse.json({ success: false }, { status: 400});
+//   }
   
-}
+// }
 
-export async function POST(
-  req: Request,
+// export async function POST(
+//   req: Request,
   
-) {
+// ) {
   
-}
+// }
 
 
 

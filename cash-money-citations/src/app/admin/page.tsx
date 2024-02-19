e155a13bcb984c17787644a4a656485ac22bb703
xyclose11 <harrisgus1@hotmail.com>
2024-02-19 16:12:46 -0600

    Add client-side code and update user role

--- cash-money-citations/src/app/admin/page.tsx
+++ cash-money-citations/src/app/admin/page.tsx
@@ -1,14 +1,31 @@
+'use client'
+
 import React from 'react';
+import { useSession } from 'next-auth/react';
 
-const AdminDashboard = () => {
-    // Add your admin dashboard logic here
+export default function AdminDashboard() {
 
+    const { data: session, update } = useSession();
+    
+    const handleUpdateUser = async () => {
+        const newSession = {
+            ...session,
+            user: {
+                ...session?.user,
+                role: "testuser"
+            },
+        }
+
+        await update(newSession)
+    }
+    
+    console.log("session", session);
     return (
         <div>
             <h1>Admin Dashboard</h1>
-            {/* Add your dashboard components and content here */}
+            <button onClick={handleUpdateUser}>Update Role For User</button>
         </div>
     );
 };
 
-export default AdminDashboard;
+// export default AdminDashboard;

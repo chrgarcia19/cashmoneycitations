d7199333846aacbb691d77eac275d09c69aa7249
xyclose11 <harrisgus1@hotmail.com>
2024-02-19 12:05:29 -0600

    Refactor middleware to handle authentication and authorization

--- cash-money-citations/src/middleware.ts
+++ cash-money-citations/src/middleware.ts
@@ -7,11 +7,36 @@ const requireAuth: string[] = ["/admin"];
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next();
   const pathname = request.nextUrl.pathname;
-  if (requireAuth.some((path) => pathname.startsWith(path))) {
+
+
+  async function getUserToken() {
     const token = await getToken({
       req: request,
       secret: process.env.SECRET,
     });
+    return token;
+  }
+
+
+  if ( request.nextUrl.pathname.startsWith('/new')) {
+    const token = await getUserToken();
+
+    if (!token) {
+      const url = new URL(`/api/auth/signin`, request.url);
+      url.searchParams.set("callbackUrl", encodeURI(request.url));
+      return NextResponse.redirect(url);
+    }
+
+
+    if (token.role === null) {
+      const url = new URL(`/`, request.url);
+      console.log("You do not have proper credentials")
+      return NextResponse.rewrite(url);
+    }
+  }
+
+  if (requireAuth.some((path) => pathname.startsWith(path))) {
+    const token = await getUserToken();
 
     //check not authenticated
     if (!token) {

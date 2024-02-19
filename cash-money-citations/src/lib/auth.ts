5c6f74f1fe591081b6256b6abe69e6f79dfe2b83
xyclose11 <harrisgus1@hotmail.com>
2024-02-19 16:12:39 -0600

    Refactor auth callbacks in auth.ts

--- cash-money-citations/src/lib/auth.ts
+++ cash-money-citations/src/lib/auth.ts
@@ -53,14 +53,18 @@ export const authConfig: NextAuthOptions = ({
     
   ],
 
-    callbacks: {
-      async jwt({token, user}) {
-        if (user) {
-          token.role = user.role;
-        }
-        return token;
-      },
-    async session({session, token}) {
+  callbacks: {
+    async jwt({token, trigger, user, session }) {
+      if (trigger === "update" && session) {
+        return { ...token, ...session?.user };
+      }
+
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({session, token, user }) {
       if (token && session.user) {
         session.user.role = token.role;
       }

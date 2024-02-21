7d8734dc11a2741e5b777729d383c1283e68b4f5
xyclose11 <harrisgus1@hotmail.com>
2024-02-21 11:49:34 -0600

    Removed unnecessary code

--- cash-money-citations/src/components/AuthButtons.tsx
+++ cash-money-citations/src/components/AuthButtons.tsx
@@ -1,46 +1,10 @@
 "use client";
-import { FaGithubSquare } from "react-icons/fa";
-import {FcGoogle} from 'react-icons/fc'
 
 import {
-  ClientSafeProvider,
-  LiteralUnion,
-  signIn,
   signOut,
 } from "next-auth/react";
 
-interface IProps {
-  providers: Record<LiteralUnion<string, string>, ClientSafeProvider>;
-}
 
-
-export function SignInGitHub({ providers }: IProps) {
-  const provider = providers.github;
-  return (
-    <>
-      {provider && (
-        <button onClick={() => signIn(provider.id)} type="button">
-          <FaGithubSquare className="mr-2" />
-          {provider.name}
-        </button>
-      )}
-    </>
-  );
-}
-
-export function SignInGoogle({ providers }: IProps) {
-  const provider = providers.google;
-  return (
-    <>
-      {provider && (
-        <button onClick={() => signIn(provider.id)} type="button">
-          <FcGoogle className="mr-2" />
-          {provider.name}
-        </button>
-      )}
-    </>
-  );
-}
 
 export function SignOut() {
   return (

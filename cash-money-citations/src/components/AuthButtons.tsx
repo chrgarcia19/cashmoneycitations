4c90006dec8a1a52c7b8b8583944141f9ceea4d5
xyclose11 <harrisgus1@hotmail.com>
2024-02-22 15:09:44 -0600

    Update signOut callbackUrl in AuthButtons component

--- cash-money-citations/src/components/AuthButtons.tsx
+++ cash-money-citations/src/components/AuthButtons.tsx
@@ -8,7 +8,7 @@ import {
 
 export function SignOut() {
   return (
-    <button className="hover:bg-slate-950 text-white" onClick={() => signOut()} type="button">
+    <button className="hover:bg-slate-950 text-white" onClick={() => signOut({ callbackUrl: '/login'})} type="button">
       Sign Out
     </button>
   );

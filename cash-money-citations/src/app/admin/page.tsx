760262507fd0b7b1acd7cadccf6f1609be9b6ae7
xyclose11 <harrisgus1@hotmail.com>
2024-02-22 18:12:57 -0600

    Delete unused component and import GetUsers component

--- cash-money-citations/src/app/admin/page.tsx
+++ cash-money-citations/src/app/admin/page.tsx
@@ -2,31 +2,16 @@ import React from 'react';
 import dbConnect from '@/utils/dbConnect';
 import User from '@/models/User';
 import AdminDashboardClient from './components/admin-dashboard';
+import GetUsers from './components/GetUsers';
 
 export default async function AdminDashboard() {
 
-    async function getUsers() {
-        await dbConnect();
-      
-        const result = await User.find({});
-        const users = result.map((doc) => {
-          const user = JSON.parse(JSON.stringify(doc));
-          return user;
-        });
-      
-        return users;
-    }
-
-    const users = await getUsers();
 
     
     return (
         <>
-            {users.map((user) => (
-                <div key={user._id}>
-                    {user.username}
-                </div>
-            ))}
+            <GetUsers />
+            {/* <SessionInfo /> */}
             <AdminDashboardClient />
         </>
     );

4c6b2263efdc6468a60a91baf952a1ec6365cb22
xyclose11 <harrisgus1@hotmail.com>
2024-02-25 11:31:07 -0600

    Fix imports and handle multiple user emails in AdminDashboardClient

--- cash-money-citations/src/app/admin/components/admin-dashboard.tsx
+++ cash-money-citations/src/app/admin/components/admin-dashboard.tsx
@@ -1,27 +1,36 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import router from 'next/router';
+import React, { startTransition, useState } from 'react';
 
 export default function AdminDashboardClient() {
-    const [userEmail, setUserEmail] = useState('');
+    const [userEmail, setUserEmail] = useState<string[]>([]);
     const router = useRouter();
 
-    function handleTextInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setUserEmail(e.target.value);
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>, email: any) {
+        if (e.target.checked) {
+            setUserEmail([...userEmail, email]);
+        } else {
+            setUserEmail(userEmail.filter(uesrEmail => userEmail !== email));
+        }
     }
 
     
-    function handleUpdateUser(e: React.FormEvent<HTMLFormElement>) {
+    async function handleUpdateUser(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         
         const form = e.target as HTMLFormElement;
         const formData = new FormData(form);
         
         // Add userEmail to the form data
-        formData.append('userEmail', userEmail);
-        
-        fetch('/api/auth/updateUser', { method: "PUT", body: formData });
-        router.refresh()
+        userEmail.forEach((email) => {
+            formData.append(`userEmail`, email);
+        })
+        await fetch('/api/auth/updateUser', { method: "PUT", body: formData });
+        startTransition(() => {
+            router.refresh();
+
+        })
     }
 
 
@@ -30,7 +39,6 @@ export default function AdminDashboardClient() {
         <div>
             <h1>Admin Dashboard</h1>
             <form method='PUT' onSubmit={handleUpdateUser}>
-                <input type="text" value={userEmail} onChange={handleTextInputChange} placeholder="User Email"   className="input w-full max-w-xs" />
 
                 <select name='userRoleSelect' > 
                     <option value="user">
--- cash-money-citations/src/app/admin/page.tsx
+++ cash-money-citations/src/app/admin/page.tsx
@@ -12,7 +12,7 @@ export default async function AdminDashboard() {
         <>
             <GetUsers />
             {/* <SessionInfo /> */}
-            <AdminDashboardClient />
+            {/* <AdminDashboardClient /> */}
         </>
     );
 };

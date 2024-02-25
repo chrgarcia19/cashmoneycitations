df92f2056da3c6d801ba30c2d8897b0b00138a92
xyclose11 <harrisgus1@hotmail.com>
2024-02-25 11:36:12 -0600

    Add POST route to update user -> Delete User

--- cash-money-citations/src/app/api/auth/updateUser/route.ts
+++ cash-money-citations/src/app/api/auth/updateUser/route.ts
@@ -23,3 +23,23 @@ export async function PUT(request: Request) {
     }
   
 }
+
+export async function POST(request: Request) {
+    await dbConnect();
+        
+    try {
+        
+        const req = await request.formData();
+
+        const email = req.get('userEmail');
+
+        // Update user
+
+        const user = await User.findOneAndDelete({email});
+        return NextResponse.json({ success: true, data: user, message: "User Updated"}, {status: 201});
+    } catch (error) {
+        console.log({ error });
+        return NextResponse.json({ success: false }, { status: 400 });
+    }
+  
+}

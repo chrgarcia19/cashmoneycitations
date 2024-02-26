98aa1c0c8692a2b5c0f724c7166b2c9eb0247671
xyclose11 <harrisgus1@hotmail.com>
2024-02-26 16:01:21 -0600

    Added image URL to OAuth sesion

--- cash-money-citations/src/lib/auth.ts
+++ cash-money-citations/src/lib/auth.ts
@@ -43,7 +43,7 @@ export const authConfig: NextAuthOptions = ({
     }),
     GithubProvider({
       async profile(profile) {
-        return Promise.resolve({ id: profile.id, role: profile.role ?? "user" });
+        return Promise.resolve({ id: profile.id, image: profile.avatar_url, role: profile.role ?? "user" });
       },
       clientId: process.env.GITHUB_APP_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
@@ -51,7 +51,7 @@ export const authConfig: NextAuthOptions = ({
 
     GoogleProvider({
       async profile(profile) {
-        return Promise.resolve({ id: profile.sub, name: profile.name, role: profile.role ?? "user" });
+        return Promise.resolve({ id: profile.sub, image: profile.picture, name: profile.name, role: profile.role ?? "user" });
       },
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
@@ -68,12 +68,14 @@ export const authConfig: NextAuthOptions = ({
 
       if (user) {
         token.role = user.role;
+        token.image = user.image;
       }
       return token;
     },
     async session({session, token, user }) {
       if (token && session.user) {
         session.user.role = token.role;
+        session.user.image = token.image;
       }
       return session;
     },

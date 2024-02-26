8ff51f4fbefd1ae5dbfef8a10d4d1b2ef442887a
chrgarcia19 <christian.garcia@valpo.edu>
2024-02-26 09:59:02 -0600

    Added background to the page

--- cash-money-citations/src/components/ReferenceGallery.tsx
+++ cash-money-citations/src/components/ReferenceGallery.tsx
@@ -20,42 +20,45 @@ export default async function HomePage() {
   const references = await getReferences();
 
   return (
-    <div className='reference-wrapper'>
-      <>
-        {references.map((reference) => (
-          <div key={reference._id}>
-              <div className="card">
-                <img src={reference.image_url} alt="Image of a Reference Cover" />
-                <h5 className="reference-name">{reference.title}</h5>
-                <div className="main-content">
-                  <p className="reference-name">{reference.title}</p>
-                  <p className="owner">Type: {reference.type}</p>
-    
-                  {/* Extra Reference Info: Year and Publisher */}
-                  <div className="likes info">
-                    <p className="label">Year</p>
-                    {reference.year}
-                  </div>
-                  <div className="dislikes info">
-                    <p className="label">Publisher</p>
-                    <ul>
-                      {reference.publisher}
-                    </ul>
-                  </div>
-                  <div className="btn-container">
-                    <Link href={{ pathname: `/${reference._id}/edit`, query: { id: reference._id } }}>
-                      <button className="btn edit bg-green-400">Edit</button>
-                    </Link>
-                    <Link href={{ pathname: `/${reference._id}/`, query: { id: reference._id } }}>
-                      <button className="btn view bg-blue-400">View</button>
-                    </Link>
+    <>  
+      <div className='reference-wrapper'>
+        <div className="fixed w-full h-screen bg-zinc-700">
+          <div className='reference-wrapper'>
+            {references.map((reference) => (
+              <div key={reference._id}>
+                  <div className="card">
+                    <img src={reference.image_url} alt="Image of a Reference Cover" />
+                    <h5 className="reference-name">{reference.title}</h5>
+                    <div className="main-content">
+                      <p className="reference-name">{reference.title}</p>
+                      <p className="owner">Type: {reference.type}</p>
+        
+                      {/* Extra Reference Info: Year and Publisher */}
+                      <div className="likes info">
+                        <p className="label">Year</p>
+                        {reference.year}
+                      </div>
+                      <div className="dislikes info">
+                        <p className="label">Publisher</p>
+                        <ul>
+                          {reference.publisher}
+                        </ul>
+                      </div>
+                      <div className="btn-container">
+                        <Link href={{ pathname: `/${reference._id}/edit`, query: { id: reference._id } }}>
+                          <button className="btn edit bg-green-400">Edit</button>
+                        </Link>
+                        <Link href={{ pathname: `/${reference._id}/`, query: { id: reference._id } }}>
+                          <button className="btn view bg-blue-400">View</button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </>
+              ))}
+          </div>
+        </div>
       </div>
-
+    </>
   )
 }

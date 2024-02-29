e3b39cc7a69815b99b23910683231d5e63351e09
xyclose11 <harrisgus1@hotmail.com>
2024-02-29 15:39:24 -0600

    Add ErrorButton component

---
+++ cash-money-citations/src/components/ErrorButton.tsx
@@ -0,0 +1,9 @@
+export function ErrorButton() {
+    return (
+        <div className="badge badge-error gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+        Email Already in Use.
+        </div>
+
+    )
+}

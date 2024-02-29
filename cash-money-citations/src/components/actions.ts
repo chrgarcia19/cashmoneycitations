e7cf59dd1d4aafb9b1aaf14909942a10756ef1bb
xyclose11 <harrisgus1@hotmail.com>
2024-02-29 16:06:11 -0600

    Revert "Refactor user creation logic to handle existing users and unknown errors"
    This reverts commit c36ef51e252610d2222975de44cd6755f0521fb2.

--- cash-money-citations/src/components/actions.ts
+++ cash-money-citations/src/components/actions.ts
@@ -36,18 +36,13 @@ export async function createUser(form: RegistrationData) {
 
       const { username, firstName, lastName, email, password } = req;
 
- 
       const userExists = await User.findOne({email});
 
-      if (userExists) {
-        return {
-          exists: true,
-          status: 409,
-          message: "Email already in use."
-        };
+      if(userExists){
+        const error = "User exists!"
+          return error;
       }
-      
-      
+
       // Hash the password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
@@ -69,9 +64,6 @@ export async function createUser(form: RegistrationData) {
       
       revalidatePath('/');
   } catch (error) {
-    return {
-      status: 409,
-      message: "Unknown user creation error. Please try again."
-    };
+    return error;
   }
 }

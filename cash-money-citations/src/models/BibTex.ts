f2281472cf0796f1c242dc69d12c198da46740ee
xyclose11 <harrisgus1@hotmail.com>
2024-03-06 08:53:35 -0600

    Add BibTex model for citations

---
+++ cash-money-citations/src/models/BibTex.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from 'mongoose';
+import { Contributor } from "./Contributor";
+
+const ContributorSchema = new mongoose.Schema<Contributor>({
+    // Define Contributor schema fields
+    contributorType: {
+      type: String
+    },
+    contributorFirstName: {
+      type: String
+    },
+    contributorLastName: {
+      type: String
+    },
+    contributorMiddleI: {
+      type: String
+    }
+});
+
+const BibTexSchema = new Schema({
+    entryType: {
+        type: String,
+        enum: [
+            'article',
+            'book',
+            'booklet',
+            'conference',
+            'inbook',
+            'incollection',
+            'inproceedings',
+            'manual',
+            'masterthesis',
+            'misc',
+            'phdthesis',
+            'proceedings',
+            'techreport',
+            'unpublished',
+        ]
+    },
+    citeKey: String,
+    address: String,
+    annote: String,
+    contributors: [ContributorSchema],
+    booktitle: String,
+    chapter: String,
+    edition: String,
+    editor: String,
+    howpublished: String,
+    institution: String,
+    journal: String,
+    month: Date,
+    note: String,
+    number: Number,
+    organization: String,
+    pages: Number,
+    publisher: String,
+    school: String,
+    series: String,
+    title: String,
+    type: String,
+    volume: String,
+    year: Date,
+    doi: String,
+    issn: String,
+    isbn: String,
+    url: String,
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});

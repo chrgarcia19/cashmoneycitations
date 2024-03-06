c9305ad67da5a3a798230e6e75afef3071816dcd
xyclose11 <harrisgus1@hotmail.com>
2024-03-06 14:37:13 -0600

    Refactor citation creation and use new CSLBibModel

--- cash-money-citations/src/app/[id]/view/actions.ts
+++ cash-money-citations/src/app/[id]/view/actions.ts
@@ -7,37 +7,43 @@ const Cite = require('citation-js')
 require('@citation-js/plugin-bibtex')
 require('@citation-js/core')
 const { plugins } = require('@citation-js/core')
+import CSLBibModel from "@/models/CSLBibTex";
+import dbConnect from "@/utils/dbConnect";
 
 export async function CreateCitation(reference: any, styleChoice: any) {
 
-    let type = "";
+    await dbConnect();
+    // let type = "";
         
-    if (reference.type === 'journal'){
-        type = 'article-journal'
-    }
-    if (reference.type === 'website'){
-        type = reference.type
-    }
-    if (reference.type === 'book'){
-        type = reference.type
-    }
-    const cslData = {
-        id: reference._id,
-        type: type,
-        title: reference.title,
-        author: reference.contributors.map((contributor: { contributorFirstName: any; contributorLastName: any; }) => ({
-        family: contributor.contributorFirstName,
-        given: contributor.contributorLastName,
-        })),
-        issued: { "date-parts": [[parseInt(reference.year, 10), reference.month ? parseInt(reference.month, 10) : 0]] },
-        publisher: reference.publisher,
-        DOI: reference.doi,
-        URL: reference.url,
-        ISBN: reference.isbn
-    };
+    // if (reference.type === 'journal'){
+    //     type = 'article-journal'
+    // }
+    // if (reference.type === 'website'){
+    //     type = reference.type
+    // }
+    // if (reference.type === 'book'){
+    //     type = reference.type
+    // }
+    // const cslData = {
+    //     id: reference._id,
+    //     type: type,
+    //     title: reference.title,
+    //     author: reference.contributors.map((contributor: { contributorFirstName: any; contributorLastName: any; }) => ({
+    //     family: contributor.contributorFirstName,
+    //     given: contributor.contributorLastName,
+    //     })),
+    //     issued: { "date-parts": [[parseInt(reference.year, 10), reference.month ? parseInt(reference.month, 10) : 0]] },
+    //     publisher: reference.publisher,
+    //     DOI: reference.doi,
+    //     URL: reference.url,
+    //     ISBN: reference.isbn
+    // };
     
+    // NEED TO CONVERT FROM USING THE REFERENCE MODEL TO THE NEW CSLBIBMODEL
+    let tempCslJson = await CSLBibModel.findById("65e8d11a59e08b35e2ea8a1f")
+    const cslJson = tempCslJson.cslJson
+
     // Create a Cite instance
-    let cslJson = Cite.input(reference);
     const citation = new Cite(cslJson);
 
     let templateName = styleChoice
@@ -53,24 +59,24 @@ export async function CreateCitation(reference: any, styleChoice: any) {
     config.templates.add(templateName, styleData)
     
     const customCitation = citation.format('bibliography', {
-        format: 'html',
+        format: 'text',
         template: templateName,
         lang: 'en-us'
     });
 
     const vanOutput = citation.format('bibliography', {
-        format: 'html',
+        format: 'text',
         template: 'vancouver',
         lang: 'en-US'
     });
     //Generate apa citation
     const apaOutput = citation.format('bibliography', {
-        format: 'html',
+        format: 'text',
         template: 'apa',
         lang: 'en-US'
     });
-    const bibtexOutput = citation.format('bibtex', {
-        format: 'html',
+    const bibtexOutput = citation.format('biblatex', {
+        format: 'text',
         template: 'bibtex',
         lang: 'en-US'
     });

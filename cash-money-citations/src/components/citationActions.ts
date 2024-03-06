1ec28337580c5572d0edaf6994e4cefe65179c1b
xyclose11 <harrisgus1@hotmail.com>
2024-03-06 09:39:17 -0600

    Refactor citationActions.ts to handle initial reference creation

--- cash-money-citations/src/components/citationActions.ts
+++ cash-money-citations/src/components/citationActions.ts
@@ -1,40 +1,50 @@
 'use server'
 
-const fs = require('fs');
 const Cite = require('citation-js')
 require('@citation-js/plugin-bibtex')
 require('@citation-js/core')
-const { plugins } = require('@citation-js/core')
 import dbConnect from "@/utils/dbConnect";
+import Citation from "@/models/Citation";
 import Reference from "@/models/Reference";
+import BibTexModel from "@/models/BibTexModel";
 
 const contentType = "application/json"
 
 // Takes reference data & converts to CSL-JSON
 function toCslJson(reference: any) {
+    console.log(reference)
     const cslJson = Cite.input(reference);
-    return JSON.stringify(cslJson);
+    // Last working on a way to validate CSL-JSON and save in Database
+    // Last last working on ensuring that the input to cslJson will always return CSL no matter if it is manual input or DOI
+    // We may need to alter the references model to fix this
+    return cslJson;
 }
 
+// Creates citeKey based off of first author last name and year
+async function InitializeCiteKey(_id: string, contributorLastName: string, year: number) {
+    try{
+        const newCiteKey = String(contributorLastName + year)
+        await BibTexModel.findByIdAndUpdate(_id, { citeKey: newCiteKey })
+    } catch(error) {
+        console.error(error)
+    }
+}
 
-export async function HandleInitialReference(form: FormData) {
+export async function HandleInitialReference(form: any) {
 
     await dbConnect();
     // Create reference entry
     try {
-        const res = await fetch("/api/references", {
-          method: "POST",
-          headers: {
-            Accept: contentType,
-            "Content-Type": contentType,
-          },
-          body: JSON.stringify(form),
-        });
-  
-        // Throw error with status code in case Fetch API req failed
-        if (!res.ok) {
-          throw new Error(res.status.toString());
-        }
+
+        const res = await Reference.create(
+            form
+        )
+        const bibResponse = await BibTexModel.create(
+            form
+        )
+
+        const {_id, contributorLastName, year} = bibResponse
+        InitializeCiteKey(_id, contributorLastName, year)
 
       } catch (error) {
         console.error(error)
@@ -42,7 +52,11 @@ export async function HandleInitialReference(form: FormData) {
 
     // Create CSL-JSON Entry
     try {
-        const citation = await 
+        const cslJson = toCslJson(form)
+        console.log(cslJson)
+        await Citation.create(
+            cslJson
+        )
     } catch(error) {
         console.error(error)
     }

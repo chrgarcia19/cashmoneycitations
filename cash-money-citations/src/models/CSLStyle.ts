1dcbaf86a8b06af307be9bc70cef02dd19eb653f
xyclose11 <harrisgus1@hotmail.com>
2024-03-08 10:25:45 -0600

    Add name and cslData properties to CSLStyleSchema

--- cash-money-citations/src/models/CSLStyle.ts
+++ cash-money-citations/src/models/CSLStyle.ts
@@ -1,7 +1,8 @@
 import mongoose, { Schema } from 'mongoose';
 
 const CSLStyleSchema = new Schema ({
-
+    name: String,
+    cslData: Object,
 })
 
 export default mongoose.models.CSLStyleModel || mongoose.model("CSLStyleModel", CSLStyleSchema); 

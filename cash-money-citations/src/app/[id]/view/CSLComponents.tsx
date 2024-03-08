f64bb617353c4757730b5b2b98b1e964cd3def24
xyclose11 <harrisgus1@hotmail.com>
2024-03-08 14:11:04 -0600

    Fix TypeScript errors and improve CSL component

---
+++ cash-money-citations/src/app/[id]/view/CSLComponents.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) =>
+fetch(url)
+    .then((res) => res.json())
+    .then((json) => json.data);
+
+interface SelectionCSLProps {
+    // Fix the typescript for onStyleChoiceChange was previosly string[]
+    onStyleChoiceChange: (styleChoices: any) => void;
+}
+
+interface SelectionCSLLocaleProps {
+    // Fix the typescript for onStyleChoiceChange was previosly string[]
+    onLocaleChoiceChange: (styleChoices: any) => void;
+}
+  
+// Maps over CSL Style selection
+export function SelectionCSL({ onStyleChoiceChange }: SelectionCSLProps) {
+const [styleChoices, setStyleChoices] = useState<string[]>([]);
+
+const {
+    data: cslStyles,
+    error,
+    isLoading,
+} = useSWR(`/api/csl/styles`, fetcher);
+
+
+if (error) return <p>Failed to load</p>;
+if (isLoading) return <p>Loading...</p>;
+if (!cslStyles) return null;
+
+const handleStyleChoiceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const styleChoice = e.target.value;
+    if (e.target.checked) {
+    setStyleChoices(prevChoices => [...prevChoices, styleChoice]);
+    onStyleChoiceChange([...styleChoices, styleChoice]);
+    } else {
+    const newChoices = styleChoices.filter(choice => choice !== styleChoice);
+    setStyleChoices(newChoices);
+    onStyleChoiceChange(newChoices);
+    }
+};
+
+return (
+    <span className="space-x-5">
+        {cslStyles.map((cslStyle: any) => (
+        <div key={cslStyle.id}>
+            <label>
+            <input
+            type="checkbox"
+            value={cslStyle.name}
+            checked={styleChoices.includes(cslStyle.name)}
+            onChange={handleStyleChoiceChange}
+            />
+            {cslStyle.name}
+            </label>
+        </div>
+        ))}
+    </span>
+);
+}
+
+
+export function SelectionLocale({ onLocaleChoiceChange }: SelectionCSLLocaleProps) {
+    const [localeChoice, setLocaleChoice] = useState<string | null>(null);
+    
+    const {
+        data: localeData,
+        error,
+        isLoading,
+    } = useSWR(`/api/csl/locales`, fetcher);
+    
+    
+    if (error) return <p>Failed to load</p>;
+    if (isLoading) return <p>Loading...</p>;
+    if (!localeData) return null;
+    
+    const handleLocaleChoiceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newLocaleChoice = e.target.value;
+        setLocaleChoice(newLocaleChoice);
+        onLocaleChoiceChange(newLocaleChoice);
+    };
+    
+    return (
+        <span className="space-x-5">
+          {localeData.map((locale: any) => (
+            <div key={locale.id}>
+              <label>
+                <input
+                  type="radio"
+                  name="localeChoice"
+                  value={locale.id}
+                  checked={localeChoice === locale.id}
+                  onChange={handleLocaleChoiceChange}
+                />
+                {locale.name}
+              </label>
+            </div>
+          ))}
+        </span>
+    );
+}

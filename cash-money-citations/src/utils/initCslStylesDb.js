0526ff63d6a8a16a08b66c0a250bb2acc0eab732
xyclose11 <harrisgus1@hotmail.com>
2024-03-08 10:25:41 -0600

    Add CSL Style import functionality to initCslStylesDb.js

--- cash-money-citations/src/utils/initCslStylesDb.js
+++ cash-money-citations/src/utils/initCslStylesDb.js
@@ -1,4 +1,5 @@
 import dbConnect from "@/utils/dbConnect";
+import CSLStyleModel from "@/models/CSLStyle";
 
 const fs = require('fs');
 const path = require('path');
@@ -6,8 +7,24 @@ const path = require('path');
 async function importCSLFiles(directoryPath) {
     await dbConnect();
 
-    // Read .csl files from directory
-    const cslFiles = fs.readdirSync(directoryPath);
-    
+    // Check if there are any CSL Styles in the database
+    const count = await CSLStyleModel.countDocuments();
 
+    if (count === 0) {
+        // Read .csl files from directory
+        const cslFiles = fs.readdirSync(directoryPath);
+
+        for (const file of cslFiles) {
+            if (path.extname(file) === '.csl') {
+                const filePath = path.join(directoryPath, file);
+                const cslData = fs.readFileSync(filePath, 'utf8');
+
+                // Insert the .csl data into the MongoDB database
+                const csl = new CSLStyleModel({ name: path.basename(file, '.csl'), cslData: cslData });
+                await csl.save();
+            }
+        }
+    }
 }
+
+export default importCSLFiles;

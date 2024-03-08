21803a98108b3376f4d7d4e8dc2109276092cb1c
xyclose11 <harrisgus1@hotmail.com>
2024-03-08 10:54:57 -0600

    Add ImportCSLStyles component to AdminDashboard

--- cash-money-citations/src/app/admin/page.tsx
+++ cash-money-citations/src/app/admin/page.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import dbConnect from '@/utils/dbConnect';
 import User from '@/models/User';
 import AdminDashboardClient from './components/admin-dashboard';
+import ImportCSLStyles from './components/ImportCSLStyles';
 import GetUsers from './components/GetUsers';
 import { getServerSession } from 'next-auth';
 import { authConfig } from '@/lib/auth';
+import importCSLFiles from '@/utils/initCslStylesDb';
 
 export default async function AdminDashboard() {
     const session = await getServerSession(authConfig);
     const currentUserEmail = session?.user?.email
 
+    async function handleCslSubmit(cslDirectory: any) {
+        'use server';
+
+        await importCSLFiles(cslDirectory);
+    }
+
         return (
         <>
             <GetUsers currentUser={currentUserEmail}/>
+            <ImportCSLStyles handleCslSubmit={handleCslSubmit}/>
             {/* <SessionInfo /> */}
             {/* <AdminDashboardClient /> */}
         </>

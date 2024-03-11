67591e932438eec36e377c3c247a3bddc81664e4
xyclose11 <harrisgus1@hotmail.com>
2024-03-11 14:06:20 -0500

    Converted CreateCitation server action to store the citation rather than return it in JSON

--- cash-money-citations/src/app/[id]/view/actions.ts
+++ cash-money-citations/src/app/[id]/view/actions.ts
@@ -11,58 +11,42 @@ import CSLBibModel from "@/models/CSLBibTex";
 import dbConnect from "@/utils/dbConnect";
 import CSLStyleModel from "@/models/CSLStyle";
 import CSLLocaleModel from "@/models/CSLLocale";
+import CitationModel from "@/models/Citation";
 
-
-export async function CreateCitation(referenceId: any, styleChoice: any) {
+export async function CreateCitation(referenceId: any, styleChoice: Array<string>) {
 
     await dbConnect();
     
     let tempCslJson = await CSLBibModel.findById(referenceId)
     const cslJson = tempCslJson.cslJson
+    const referenceTitle = tempCslJson.title
 
     // Create a Cite instance
     const citation = new Cite(cslJson);
 
-    let templateName = styleChoice
-
     // Retrieve CSL Style from database
+    for (const style in styleChoice) {
+        const templateName = styleChoice[style]
 
-    const styleData = await CSLStyleModel.findOne({
-        name: templateName,
-    }).exec()
-    // Retrieve CSL Style from root server
-    // const stylePath = path.resolve(`./csl_styles/${templateName}`)
-    // const styleData = fs.readFileSync(stylePath, 'utf8');
-
+        const styleData = await CSLStyleModel.findOne({
+            name: templateName,
+        }).exec()
+            
+        const config = plugins.config.get('@csl')
         
-    const config = plugins.config.get('@csl')
+        config.templates.add(templateName, styleData?.cslData)
+        
+        const customCitation = citation.format('bibliography', {
+            format: 'text',
+            template: templateName,
+            lang: 'en-us'
+        });
 
-    config.templates.add(templateName, styleData?.cslData)
+        await CitationModel.create({
+            name: templateName + " " + referenceTitle,
+            CitationData: customCitation
+        })
     
-    const customCitation = citation.format('bibliography', {
-        format: 'text',
-        template: templateName,
-        lang: 'en-us'
-    });
+    }
 
-    const vanOutput = citation.format('bibliography', {
-        format: 'text',
-        template: 'vancouver',
-        lang: 'en-US'
-    });
-    //Generate apa citation
-    const apaOutput = citation.format('bibliography', {
-        format: 'text',
-        template: 'apa',
-        lang: 'en-US'
-    });
-    const bibtexOutput = citation.format('biblatex', {
-        format: 'text',
-        template: 'bibtex',
-        lang: 'en-US'
-    });
-
-    // Implement the logic to display or prepare the citation for download
-    const citationData = JSON.stringify({ van: vanOutput, apa: apaOutput, bibtex: customCitation });
-    return citationData;
 }

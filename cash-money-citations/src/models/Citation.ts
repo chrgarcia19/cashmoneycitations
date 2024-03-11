91f3872843f5c9ec429639d917bd779a9fd43247
xyclose11 <harrisgus1@hotmail.com>
2024-03-11 15:18:52 -0500

    Add language field to Citation model

--- cash-money-citations/src/models/Citation.ts
+++ cash-money-citations/src/models/Citation.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 const CitationSchema = new Schema ({
     name: String,
     CitationData: Object,
+    language: String,
 })
 
 export default mongoose.models.CitationModel || mongoose.model("CitationModel", CitationSchema); 

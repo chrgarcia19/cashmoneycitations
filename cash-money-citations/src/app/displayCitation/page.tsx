9e41ef5563cb193c5cebc9ef38ed69a9099466c5
xyclose11 <harrisgus1@hotmail.com>
2024-03-12 12:38:42 -0500

    Refactor citation display component to take reference ID as SearchParam

--- cash-money-citations/src/app/displayCitation/page.tsx
+++ cash-money-citations/src/app/displayCitation/page.tsx
@@ -1,6 +1,32 @@
-"use client"
+import { GetCitations } from './actions';
 
-import { useEffect, useState } from 'react';
+async function DisplayCitations({ referenceId }: any) {
+  const citationList = await GetCitations(referenceId.citation);
+  const citations = citationList?.map((doc) => {
+    const citation = JSON.parse(JSON.stringify(doc));
+    return citation;
+  })
+
+  return (
+    <>
+      
+        {citations?.map((citation) => (
+          <div key={citation.id}>
+            {citation.CitationData}
+            <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
+            <div className='mt-3'>{citation.name}</div>
+            {/* <button onClick={() => copyToClipboard(citationData.van)}> */}
+            {/* <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
+            </button> */}
+            </div>
+          </div>
+        ))}
+      
+    </>
+  )
+
+}
+
 
 export default function citationDisplay({
   params,
@@ -9,44 +35,28 @@ export default function citationDisplay({
   params: { slug: string }
   searchParams: { [key: string]: string | string | undefined }
 }){
-  const [citationData, setCitationData] = useState({ van: '', apa: '', bibtex: '' });
-  useEffect(() => {
-    const citation = searchParams.citation;
-    if (citation) {
-      const parsedData = JSON.parse(decodeURIComponent(citation));
-      setCitationData(parsedData);
-    }
-  }, [searchParams]);
-
-  const copyToClipboard = (text : string) => {
-    navigator.clipboard.writeText(text).then(() => {
-      alert('Copied to clipboard!');
-    }).catch(err => {
-      console.error('Failed to copy: ', err);
-    });
-  };
-
+  
   return (
     <div className='flex flex-row items-start w-screen'>
-     <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
-        <div className='mt-3'>Vancouver Citation: {citationData.van}</div>
-        <button onClick={() => copyToClipboard(citationData.van)}>
-        <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
-        </button>
-      </div>
-      <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
-        <p className='mt-3'>APA Citation: {citationData.apa}</p>
-        <button onClick={() => copyToClipboard(citationData.apa)}>
-        <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
-        </button>
-      </div>
-      <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
-        <p className='mt-3'>BibTex: {citationData.bibtex}</p>
-        <button onClick={() => copyToClipboard(citationData.bibtex)}>
-        <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
-        </button>
-      </div>
-
+      <DisplayCitations referenceId={searchParams} />
     </div>
   )
 };
+    //  <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
+    //     <div className='mt-3'>Vancouver Citation: {citationData.van}</div>
+    //     <button onClick={() => copyToClipboard(citationData.van)}>
+    //     <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
+    //     </button>
+    //   </div>
+    //   <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
+    //     <p className='mt-3'>APA Citation: {citationData.apa}</p>
+    //     <button onClick={() => copyToClipboard(citationData.apa)}>
+    //     <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
+    //     </button>
+    //   </div>
+    //   <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
+    //     <p className='mt-3'>BibTex: {citationData.bibtex}</p>
+    //     <button onClick={() => copyToClipboard(citationData.bibtex)}>
+    //     <img className='copy-icon' src="/copy-icon.svg" alt="Copy" width="30" height="30" />
+    //     </button>
+    //   </div>

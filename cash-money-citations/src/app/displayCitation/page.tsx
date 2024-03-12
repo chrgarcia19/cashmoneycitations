03ad488771095fc9610dec6c44e94b9d52ae0bd9
xyclose11 <harrisgus1@hotmail.com>
2024-03-12 13:10:59 -0500

    Added basic Tailwind CSS styling to the citation display table

--- cash-money-citations/src/app/displayCitation/page.tsx
+++ cash-money-citations/src/app/displayCitation/page.tsx
@@ -10,16 +10,34 @@ async function DisplayCitations({ referenceId }: any) {
 
   return (
     <>
-      
-        {citations?.map((citation) => (
-          <div key={citation.id}>
-            {citation.CitationData}
-            <div className='flex flex-row bg-green-200 rounded-lg p-4 m-4'>
-            <div className='mt-3'>{citation.style}</div>
-            <CopyToClipboard citationData={citation.CitationData} />
-            </div>
-          </div>
-        ))}
+      <div className='px-20'>
+        <table className="min-w-full divide-y divide-gray-200 border border-gray-300">
+          <thead className='bg-gray-50'>
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation Style</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Copy</th>
+            </tr>
+            
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {citations?.map((citation) => (
+              <tr key={citation.id} className='hover:bg-gray-100'>
+                <td className="px-6 py-4 text-center text-s">
+                  {citation.style}
+                </td>
+                <td className="px-6 py- text-center text-xs">
+                  {citation.CitationData}
+                </td>
+                <td className="px-6 py-4 text-center">
+                  <CopyToClipboard citationData={citation.CitationData} />
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       
     </>
   )

b7c7a94b4e367e36dfc65f360bcbf2bf11eaac1b
xyclose11 <harrisgus1@hotmail.com>
2024-03-13 11:58:51 -0500

    Refactor file upload in ImportCSLStyles component

--- cash-money-citations/src/app/admin/components/ImportCSLStyles.tsx
+++ cash-money-citations/src/app/admin/components/ImportCSLStyles.tsx
@@ -1,24 +1,29 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+
+const contentType = "application/x-zip-compressed";
 
 function ImportCSLStyles({handleCslSubmit}: any) {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    
+    const router = useRouter();
     
     function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
         const file = e.target.files ? e.target.files[0] : null;
         if (file) {
           // Check the file extension for the file type
           const extension = file.name.split('.').pop();
-          if (extension !== 'csl') {
-            alert('Invalid file type. Please select a .csl file.');
+          if (extension !== 'zip') {
+            alert('Invalid file type. Please select a .zip file.');
             return;
           }
       
-          // Check the file size (5MB in this example)
-          const maxSizeInBytes = 5 * 1024 * 1024;
+          // Check the file size (25MB in this example)
+          const maxSizeInBytes = 25 * 1024 * 1024;
           if (file.size > maxSizeInBytes) {
-            alert('File is too large. Please select a file smaller than 5MB.');
+            alert('File is too large. Please select a file smaller than 25MB.');
             return;
           }
       
@@ -26,24 +31,14 @@ function ImportCSLStyles({handleCslSubmit}: any) {
         }    
     }
 
-    function handleSubmit(e: React.FormEvent) {
+
+    async function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
         if (selectedFile) {
-          const reader = new FileReader();
-          reader.onload = function(event) {
-            const fileContents = event.target?.result;
-            const fileData = {
-              name: selectedFile.name,
-              contents: fileContents
-            };
-            handleCslSubmit(fileData)
-                .then((response: any) => {
-                    if (response.status === "error") {
-                        alert(response.message)
-                    }
-                })
-          };
-          reader.readAsText(selectedFile);
+          const formData = new FormData();
+          formData.append('file', selectedFile);
+
+          handleCslSubmit(formData);
         }
     }
     
@@ -54,7 +49,7 @@ function ImportCSLStyles({handleCslSubmit}: any) {
                 <label htmlFor='file-upload' className="cursor-pointer">
                     <span className="mr-2 text-blue-600 hover:text-blue-800">Upload a .csl file</span>
                 </label>
-                <input id="file-upload" type="file" accept=".csl" onChange={handleFileChange} className="hidden" />
+                <input id="file-upload" type="file" accept=".zip" onChange={handleFileChange} className="hidden" />
 
             </div>
             <button type="submit" className="px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded">Import CSL Styles</button>

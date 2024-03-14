e135a84b2f218505eb98423831994720b0c7ec85
xyclose11 <harrisgus1@hotmail.com>
2024-03-14 11:20:21 -0500

    Added validation and proper features to accomodate zip files for locales

--- cash-money-citations/src/utils/initLocaleDb.js
+++ cash-money-citations/src/utils/initLocaleDb.js
@@ -11,18 +11,33 @@ async function localeExists(localeName) {
 }
 
 async function importLocaleFiles(fileData) {
-    // Remove "locales" & "xml" from filename
-    let fileName = fileData.name;
-    fileName = fileName.replace("locales-", "");
-    fileName = fileName.replace(".xml", "");
 
-    // Check to see if locale already exists
-    if (await localeExists(fileName)) {
-        return { status: "error", message: 'Locale already exists' };
+    async function validateFileType(fileData) {
+        let fileName = fileData.name;
+        if (!fileName.endsWith(".xml") && !fileName.startsWith("locales-")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    if (await !validateFileType(fileData)) {
+        return { status: "error", message: 'Not a Locale' };
     } else {
-        const locale = new CSLLocaleModel({ name: fileName, localeData: fileData.contents})
-        await locale.save()   
-        return { status: "success", message: 'Locale added' };
+        // Remove "locales" & "xml" from filename
+        let fileName = fileData.name;
+        fileName = fileName.replace("locales-", "");
+        fileName = fileName.replace(".xml", "");
+    
+        // Check to see if locale already exists
+        if (await localeExists(fileName)) {
+            return { status: "error", message: 'Locale already exists' };
+        } else {
+            const locale = new CSLLocaleModel({ name: fileName, localeData: fileData.contents})
+            await locale.save()   
+            return { status: "success", message: 'Locale added' };
+        }
+
     }
 
 }

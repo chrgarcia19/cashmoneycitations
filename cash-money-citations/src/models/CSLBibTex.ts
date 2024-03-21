3252bd40773539a1d6ff7b5f1307fc33a4684c70
xyclose11 <harrisgus1@hotmail.com>
2024-03-21 13:54:37 -0500

    Created interfaces for CSLBibSchema to be used in reference-table & reference-gallery

--- cash-money-citations/src/models/CSLBibTex.ts
+++ cash-money-citations/src/models/CSLBibTex.ts
@@ -35,6 +35,64 @@ const referencesUsedSchema = new mongoose.Schema({
   journalTitle: String
 })
 
+interface ReferencesUsed {
+  referenceId: String,
+  doiAssertedBy: String,
+  firstPage: String,
+  doi: String,
+  volume: String,
+  author: String,
+  year: String,
+  journalTitle: String
+}
+
+export interface CSLBibInterface extends mongoose.Document {
+  entryType: "article" | "article-journal" | "article-magazine" | "article-newspaper" | "bill" | "book" | "broadcast" | "chapter" | "classic" | "collection" | "dataset" | "document" | "entry" | "entry-dictionary" | "entry-encyclopedia" | "event" | "figure" | "graphic" | "hearing";
+    address: String,
+    annote: String,
+    contributors: Contributor[],
+    website_title: String,
+    chapter: String,
+    edition: String,
+    editor: String,
+    howpublished: String,
+    institution: String, 
+    journal: String, //Journal Title
+    date: Date; // Date published
+    urldate: Date; // Date accessed
+    eventdate: Date;
+    origdate: Date;
+    month: string;
+    year: string;
+    note: string;
+    number: number;
+    organization: string;
+    pages: string;
+    publisher: string;
+    school: string;
+    series: string;
+    volumes: number;
+    shorttitle: string;
+    title: string[];
+    type: string;
+    volume: string;
+    doi: string;
+    issn: string[];
+    issnType: object[];
+    isbn: string;
+    url: string;
+    rights: string;
+    runningTime: string;
+    format: string;
+    cslJson: object;
+    image_url: string;
+    issue: string;
+    abstract: string;
+    apiSource: string;
+    subject: string[];
+    referencesUsed: ReferencesUsed[];
+    citationIdList: string[];
+}
 
 const CSLBibSchema = new Schema({
     entryType: {

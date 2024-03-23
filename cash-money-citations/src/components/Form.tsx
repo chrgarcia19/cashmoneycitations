6baaed5cac2caff03b27cdf293750a19f9ff5844
chrgarcia19 <christian.garcia@valpo.edu>
2024-03-23 15:42:53 -0500

    Made progress on refactoring dynamic forms into one form

--- cash-money-citations/src/components/Form.tsx
+++ cash-money-citations/src/components/Form.tsx
@@ -5,14 +5,7 @@ import ContributorForm from "./ContributorForm";
 import { Contributor } from "@/models/Contributor";
 import { HandleManualReference } from "./citationActions";
 import { useRouter, useSearchParams } from "next/navigation";
-import { mutate } from "swr";
 import { EditReference } from "./editReferenceActions";
-import WebForm from "./form-components/WebForm";
-import BookForm from "./form-components/BookForm";
-import JournalForm from "./form-components/JournalForm";
-import MagazineForm from "./form-components/MagazineForm";
-import NewspaperForm from "./form-components/NewspaperForm";
-import DatabaseForm from "./form-components/DatabaseForm";
 import FormField from "./FormField";
 import DatePicker from "./DatePicker";
 
@@ -69,27 +62,27 @@ interface FormData {
   entryType: {
     type: string,
     enum: EntryType
-  }
-  address: string;
-  annote: string;
-  contributors: Contributor[];
-  website_title: string;
-  chapter: string;
-  edition: string;
-  editor: string;
-  howpublished: string;
-  institution: string;
-  journal: string;
+  }//
+  address: string;//
+  annote: string; //
+  contributors: Contributor[];//
+  website_title: string;//
+  chapter: string;//
+  edition: string;//
+  editor: string;//
+  howpublished: string;//
+  institution: string;//
+  journal: string;//
   /*The next three fields are later
   converted into a date object*/
-  month_published: string;
-  day_published: string;
-  year_published: string;
+  month_published: string;//
+  day_published: string;//
+  year_published: string;//
   /*The next three fields are later
   converted into a date object*/
-  month_accessed: string;
-  day_accessed: string;
-  year_accessed: string;
+  month_accessed: string;//
+  day_accessed: string;//
+  year_accessed: string;//
   /*The next three fields are later
   converted into a date object*/
   month_event: string;
@@ -114,16 +107,16 @@ interface FormData {
   title: [string];
   type: string;
   volume: number;
-  doi: string;
+  doi: string;//
   issn: [string];
-  isbn: string;
+  isbn: string;//
   url: string;
   rights: string;
-  runningTime: string;
+  running_time: string;
   format: string;
-  image_url: string;
+  image_url: string;//
   issue: string;
-  apiSource: string;
+  api_source: string;
 }
 
 interface Error {
@@ -148,132 +141,54 @@ const Form = ({ formId, referenceForm, forNewReference = true }: Props) => {
   const router = useRouter();
   const contentType = "application/json";
 
-  const webData = {
-    type: "website",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    website_title: "",
-    website_url: "",
-    month_accessed: "",
-    day_accessed: "",
-    year_accessed: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    publisher: "",
-  };
-
-  const bookData = {
-    type: "book",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    volume: "",
-    edition: "",
-    date: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    publisher: "",
-    city: "",
-    state: "",
-    isbn: "",
-  };
-
-  const journalData = {
-    type: "journal",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    journal_title: "",
-    volume: "",
-    issue: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    start_page: "",
-    end_page: "",
-    doi: "",
-    issn: "",
-  };
-
-  const magazineData = {
-    type: "magazine",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    magazine_title: "",
-    volume: "",
-    issue: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    start_page: "",
-    end_page: "",
-    doi: "",
-    issn: "",
-  };
-
-  const newspaperData = {
-    type: "newspaper",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    newspaper_title: "",
-    edition: "",
-    section: "",
-    city: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    start_page: "",
-    end_page: "",
-    issn: "",
-  }
-
-  const databaseData = {
-    type: "database",
-    citekey: "",
-    image_url: "",
-    contributors: new Array<Contributor>(),
-    title: "",
-    library: "",
-    database: "",
-    database_url: "",
-    city: "",
-    month_accessed: "",
-    day_accessed: "",
-    year_accessed: "",
-    month_published: "",
-    day_published: "",
-    year_published: "",
-    service: "",
-    issn: "",
-  }
-
   /*Set initial state to website so the page is not blank*/
   const [form, setForm] = useState({
-    entryType: referenceForm.entryType,
-    //type: referenceForm.type,
-    title: referenceForm.title,
-    contributors: referenceForm.contributors,
-    publisher: referenceForm.publisher,
-    year: referenceForm.year,
-    month: referenceForm.month,
+    entryType: "article",
     address: referenceForm.address,
+    annote: referenceForm.annote,
+    contributors: referenceForm.contributors,
+    website_title: referenceForm.website_title,
+    chapter: referenceForm.chapter,
     edition: referenceForm.edition,
-    volume: referenceForm.volume,
-    isbn: referenceForm.isbn,
-    doi: referenceForm.doi,
-    pages: referenceForm.pages,
+    editor: referenceForm.editor,
+    howpublished: referenceForm.howpublished,
+    institution: referenceForm.institution,
     journal: referenceForm.journal,
+    month_published: referenceForm.month_published,
+    day_published: referenceForm.day_published,
+    year_published: referenceForm.year_published,
+    month_accessed: referenceForm.month_accessed,
+    day_accessed: referenceForm.day_accessed,
+    year_accessed: referenceForm.year_accessed,
+    month_event: referenceForm.month_event,
+    day_event: referenceForm.day_event,
+    month_orig: referenceForm.month_orig,
+    day_orig: referenceForm.day_orig,
+    year_orig: referenceForm.year_orig,
+    month: referenceForm.month,
+    year: referenceForm.year,
+    note: referenceForm.note,
+    number: referenceForm.number,
+    organization: referenceForm.organization,
+    pages: referenceForm.pages,
+    publisher: referenceForm.publisher,
+    school: referenceForm.school,
+    series: referenceForm.series,
+    volumes: referenceForm.volumes,
+    short_title: referenceForm.short_title,
+    title: referenceForm.title,
+    type: referenceForm.type,
+    volume: referenceForm.volume,
+    doi: referenceForm.doi,
+    issn: referenceForm.issn,
+    isbn: referenceForm.isbn,
+    url: referenceForm.url,
+    rights: referenceForm.rights,
+    running_time: referenceForm.running_time,
+    format: referenceForm.format,
     image_url: referenceForm.image_url,
+    issue: referenceForm.issue,
+    api_source: referenceForm.api_source
   });
 
   //Handling contributor stuff
@@ -328,47 +243,96 @@ const Form = ({ formId, referenceForm, forNewReference = true }: Props) => {
       </div>
       <br/>
         <form id={formId} onSubmit={handleSubmit}>
-          <select id="reference-select-entrytype" name="entryType" onChange={handleChange}>
+          <select id="reference-select-entrytype" 
+            name="entryType" 
+            defaultValue={form.entryType}
+            onChange={handleChange}>
             {Object.values(EntryType).map((value) => (
               <option key={value} value={value}>
                 {value}
               </option>
             ))}
           </select>
-          <div className="flex items-center justify-center pt-10">
-            <ContributorForm updateFormData={updateFormData} contributors={form.contributors}/>
-          </div>
-          <FormField labelText={"Image URL (Optional)"} fieldName={"image_url"} fieldValue={form.image_url} fieldType={"url"} fieldPlaceholder={"Image URL"} handleChange={handleChange} />
-          <FormField labelText={"Title"} fieldName={"title"} fieldValue={form.title} fieldType={"text"} fieldPlaceholder={"Title"} handleChange={handleChange} />
-          <FormField labelText={"Publisher"} fieldName={"publisher"} fieldValue={form.publisher} fieldType={"text"} fieldPlaceholder={"Publisher"} handleChange={handleChange} />
-          <DatePicker masterLabelText={"Date Published (Month, Day, Year)"} labelText={["Month", "Day", "Year"]} fieldName={["month", "day", "year"]} fieldValue={[form.month, "", form.year]} fieldType={"text"} fieldPlaceholder={"Pick a Year"} handleChange={handleChange} />
-          <DatePicker masterLabelText={"Date Accessed (Month, Day, Year)"} labelText={["Month", "Day", "Year"]} fieldName={["month", "day", "year"]} fieldValue={[form.month, "", form.year]} fieldType={"text"} fieldPlaceholder={"Pick a Year"} handleChange={handleChange} />
+
+          {/*Image URL Field for References*/}
+          {form.entryType && (
+            <FormField labelText={"Image URL (Optional)"} fieldName={"image_url"} fieldValue={form.image_url} fieldType={"url"} fieldPlaceholder={"Image URL"} handleChange={handleChange} />
+          )}
+
+          {/*Title field*/}
+          {form.entryType && (
+            <FormField labelText={"Title"} fieldName={"title"} fieldValue={form.title} fieldType={"text"} fieldPlaceholder={"Title"} handleChange={handleChange} />
+          )}
+
+          {/*Contributors Field*/}
+          {form.entryType && (
+            <div className="flex items-center justify-center pt-10">
+              <ContributorForm updateFormData={updateFormData} contributors={form.contributors}/>
+            </div>
+          )}
+
+          {/*Series Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"Series"} fieldName={"series"} fieldValue={form.series} fieldType={"text"} fieldPlaceholder={"Series"} handleChange={handleChange} />
+          )}
+
+          {/*Pages Field*/}
+          {(form.entryType == "book" || form.entryType == "article-journal") && (
+            <FormField labelText={"Number of Pages"} fieldName={"pages"} fieldValue={form.pages} fieldType={"text"} fieldPlaceholder={"Number of Pages"} handleChange={handleChange} />
+          )}
+
+          {/*Volume Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"Volume"} fieldName={"volume"} fieldValue={form.volume} fieldType={"text"} fieldPlaceholder={"Volume"} handleChange={handleChange} />
+          )}
+
+          {/*Number of Volumes Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"Number of Volumes"} fieldName={"volumes"} fieldValue={form.volumes} fieldType={"text"} fieldPlaceholder={"Number of Volumes"} handleChange={handleChange} />
+          )}
+
+          {/*Edition Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"Edition"} fieldName={"edition"} fieldValue={form.edition} fieldType={"text"} fieldPlaceholder={"Edition"} handleChange={handleChange} />
+          )}
+
+          {/*Publisher Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"Publisher"} fieldName={"publisher"} fieldValue={form.publisher} fieldType={"text"} fieldPlaceholder={"Publisher"} handleChange={handleChange} />
+          )}
+
+          {/*Date Field*/}
+          {(form.entryType == "book") && (
+            <DatePicker masterLabelText={"Date Published (Month, Day, Year)"} labelText={["Month", "Day", "Year"]} fieldName={["month", "day", "year"]} fieldValue={[form.month_published, form.day_published, form.year_published]} fieldType={"text"} fieldPlaceholder={"Pick a Year"} handleChange={handleChange} />
+          )}
+
+          {/*ISBN Field*/}
+          {(form.entryType == "book") && (
+            <FormField labelText={"International Standard Book Number (ISBN)"} fieldName={"isbn"} fieldValue={form.isbn} fieldType={"text"} fieldPlaceholder={"ISBN"} handleChange={handleChange} />
+          )}
+  
+
+           
+          <DatePicker masterLabelText={"Date Accessed (Month, Day, Year)"} labelText={["Month", "Day", "Year"]} fieldName={["month", "day", "year"]} fieldValue={[form.month_accessed, form.day_accessed, form.year_accessed]} fieldType={"text"} fieldPlaceholder={"Pick a Year"} handleChange={handleChange} />
+          <FormField labelText={"Address"} fieldName={"address"} fieldValue={form.address} fieldType={"text"} fieldPlaceholder={"Address"} handleChange={handleChange} />
+          <FormField labelText={"Digital Object Identifier (DOI)"} fieldName={"doi"} fieldValue={form.doi} fieldType={"text"} fieldPlaceholder={"DOI"} handleChange={handleChange} />
+          <FormField labelText={"Journal"} fieldName={"journal"} fieldValue={form.journal} fieldType={"text"} fieldPlaceholder={"Journal"} handleChange={handleChange} />
+          <FormField labelText={"Annotation"} fieldName={"annote"} fieldValue={form.annote} fieldType={"text"} fieldPlaceholder={"Annotation"} handleChange={handleChange} />
+          <FormField labelText={"Website Title"} fieldName={"website_title"} fieldValue={form.website_title} fieldType={"text"} fieldPlaceholder={"Website Title"} handleChange={handleChange} />
+          <FormField labelText={"Chapter"} fieldName={"chapter"} fieldValue={form.chapter} fieldType={"text"} fieldPlaceholder={"Chapter"} handleChange={handleChange} />
+          <FormField labelText={"Editor"} fieldName={"editor"} fieldValue={form.editor} fieldType={"text"} fieldPlaceholder={"Editor"} handleChange={handleChange} />
+          <FormField labelText={"Publisher Type"} fieldName={"howpublished"} fieldValue={form.howpublished} fieldType={"text"} fieldPlaceholder={"Publisher Type"} handleChange={handleChange} />
+          <FormField labelText={"Institution"} fieldName={"institution"} fieldValue={form.institution} fieldType={"text"} fieldPlaceholder={"Institution"} handleChange={handleChange} />
+
+
+
+          <button
+            type="submit" 
+            className="btn bg-green-500 hover:bg-green-900 text-white">
+            Submit
+          </button> 
         </form>
-    
-            {/*{form.entryType == "webpage" && (
-              <WebForm formID={"add-web-reference"} webForm={webData}  />
-            )}
-
-            {form.entryType == "book" && (
-              <BookForm formID={"add-book-reference"} bookForm={bookData} />
-            )}
-
-            {form.entryType == "article-journal" && (
-              <JournalForm formID={"add-journal-reference"} journalForm={journalData} />
-            )}
-
-            {form.entryType == "article-magazine" && (
-              <MagazineForm formID={"add-magazine-reference"} magazineForm={magazineData} />
-            )}
-
-            {form.entryType == "article-newspaper" && (
-              <NewspaperForm formID={"add-newspaper-reference"} newspaperForm={newspaperData} />
-            )}
-
-            {form.entryType == "dataset" && (
-              <DatabaseForm formID={"add-database-reference"} databaseForm={databaseData} />
-            )}*/}
-        </div>
+     </div>
     </>
   );
 };

0f68b88b879d51a4911c2a9ad1035e8279d2ea4d
xyclose11 <harrisgus1@hotmail.com>
2024-03-23 10:59:06 -0500

    Reverted change to use the CSLBibModel as opposed to the User model to retrieve Citations

--- cash-money-citations/src/app/displayCitation/actions.ts
+++ cash-money-citations/src/app/displayCitation/actions.ts
@@ -1,34 +1,19 @@
 'use server'
 
-import dbConnect from "@/utils/dbConnect";
 import CSLBibModel from "@/models/CSLBibTex";
 import CitationModel from "@/models/Citation";
-import User from "@/models/User";
-import { authConfig } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
 
-async function getUserIdServer() {
-    await dbConnect();
-
-    const session = await getServerSession(authConfig);
-    const userId = session?.user?.id ?? '';
-    
-    return await User.findById(userId);
-}
-
-export async function DeleteCitation(referenceId: string, citationId: string) {
-    const user = await getUserIdServer();
-
-
+export async function DeleteCitation(citationId: string) {
+    await CitationModel.findOneAndDelete({ _id: citationId });
 }
 
 export async function GetCitations(referenceId: any) {
-    const user = await getUserIdServer();
 
+    const reference = await CSLBibModel.findById(referenceId);
 
     const citationList = [];
 
-    for (const citationId of user.ownedCitations) {
+    for (const citationId of reference.citationIdList) {
         let citation = await CitationModel.findById(citationId);
 
         citationList.push(citation)

c3216c0429e0e6c979c37319c7b18eee465de2d9
xyclose11 <harrisgus1@hotmail.com>
2024-03-23 14:53:45 -0500

    Fixed error where GetCitations passed a nested Mongo Document to React as Props

--- cash-money-citations/src/app/displayCitation/actions.ts
+++ cash-money-citations/src/app/displayCitation/actions.ts
@@ -2,24 +2,33 @@
 
 import CSLBibModel from "@/models/CSLBibTex";
 import CitationModel from "@/models/Citation";
+import mongoose from "mongoose";
 
 export async function DeleteCitation(citationId: string) {
     await CitationModel.findOneAndDelete({ _id: citationId });
 }
 
-export async function GetCitations(referenceId: any) {
-
-    const reference = await CSLBibModel.findById(referenceId);
-
+export async function GetCitations(referenceId: string) {
+    let reference = await CSLBibModel.findOne({_id: referenceId});
+    reference = reference.toObject(); // Convert to plain JavaScript object
     const citationList = [];
 
     for (const citationId of reference.citationIdList) {
         let citation = await CitationModel.findById(citationId);
+        citation = citation.toObject(); // Convert to plain JavaScript object
 
-        citationList.push(citation)
+        // If citation contains nested Mongoose documents, convert them to plain JavaScript objects
+        for (const key in citation) {
+            if (key === '_id') { // Convert the _id ObjectId to a string
+                citation[key] = citation[key].toString();
+            } else if (citation[key] instanceof mongoose.Document) {
+                citation[key] = citation[key].toObject();
+            }
+        }
+
+        citationList.push(citation);
+
     }
 
     return citationList;
-
-
 }

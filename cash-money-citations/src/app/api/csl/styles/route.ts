00a3c50e61540e97ab7e58e7af91eaed7f66d4fd
xyclose11 <harrisgus1@hotmail.com>
2024-03-24 12:07:30 -0500

    Changed csl API GET logic to retrieve only the styles from the userStyleList

--- cash-money-citations/src/app/api/csl/styles/route.ts
+++ cash-money-citations/src/app/api/csl/styles/route.ts
@@ -1,19 +1,31 @@
 import { NextResponse, NextRequest } from "next/server";
 import dbConnect from "@/utils/dbConnect";
 import CSLStyleModel from "@/models/CSLStyle";
+import UserStyleList from "@/models/UserStyleList";
+import { authConfig } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
-
-
   await dbConnect();
 
   const id = request.body;
   
   try {
+    const session = await getServerSession(authConfig);
+    const userId = session?.user?.id ?? '';
+
+    const userStyles = await UserStyleList.findOne({
+      userId: userId
+    });
+
+    const CSLTitles = userStyles.defaultStyles;
+
+    const cslStyleData = await CSLStyleModel.find({
+      title: { $in: CSLTitles }
+    });
     
-    const cslStyleData = await CSLStyleModel.find(); /* find all the data in our database */
     return NextResponse.json({ success: true, data: cslStyleData, message: id }, { status: 200});
   } catch (error) {
     return NextResponse.json({ success: false , data: id}, { status: 400});

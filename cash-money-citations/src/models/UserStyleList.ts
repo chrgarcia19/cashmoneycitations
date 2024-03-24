f94f202ec243d1abdd03d6d87e6c79c354c50d8d
xyclose11 <harrisgus1@hotmail.com>
2024-03-24 11:54:32 -0500

    Add defaultStyles to UserStyleListSchema

--- cash-money-citations/src/models/UserStyleList.ts
+++ cash-money-citations/src/models/UserStyleList.ts
@@ -10,29 +10,22 @@ const UserStyleListSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'CitationModel'
     }],
+    defaultStyles: [{
+        type: String,
+        default: [
+            "Modern Language Association 7th edition",
+            "Modern Language Association 8th edition",
+            "Modern Language Association 9th edition",
+            "American Psychological Association 7th edition",
+            "Chicago Manual of Style 17th edition (author-date)",
+            "Chicago Manual of Style 17th edition (full note)",
+            "Chicago Manual of Style 17th edition (note)",
+            "ACM SIGGRAPH",
+            "IEEE",
+        ]
+    }],
 });
 
-// Define the default styles
-const defaultStyles = ['Default Style 1', 'Default Style 2']; // replace with your default style titles
 
-UserStyleListSchema.pre('save', async function(next) {
-    // `this` refers to the UserStyleList document being saved
-    const userStyleList = this;
-
-    // Get all styles from the CitationModel collection
-    const styles = await mongoose.model('CitationModel').find();
-
-    // Check each style to see if it matches a default style
-    for (const style of styles) {
-        if (defaultStyles.includes(style.title)) {
-            // If the style matches a default style and it's not already in the styleList, add it
-            if (!userStyleList.styleList.includes(style._id)) {
-                userStyleList.styleList.push(style._id);
-            }
-        }
-    }
-
-    next();
-});
 
 export default mongoose.models.UserStyleListModel || mongoose.model("UserStyleListModel", UserStyleListSchema);

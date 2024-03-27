f508a82bf135c3bddb4f36fef3f85ad37aed610e
xyclose11 <harrisgus1@hotmail.com>
2024-03-27 14:10:51 -0500

    Refactor citation display component layout

--- cash-money-citations/src/app/displayCitation/page.tsx
+++ cash-money-citations/src/app/displayCitation/page.tsx
@@ -13,26 +13,29 @@ export default function citationDisplay({
   const [citations, setCitations] = useState([]);
 
   return (
-    <div className='flex flex-row items-start w-screen'>
-      {/* <CitationChoice referenceId={searchParams} /> */}
+    <div className='center-content'>
+      <div className='flex flex-row items-start w-screen'>
+        {/* <CitationChoice referenceId={searchParams} /> */}
 
-      <div className='px-20'>
-        <CitationChoice referenceId={referenceId} citations={citations} setCitations={setCitations} />
-        <table className="min-w-full divide-y divide-gray-200 border border-gray-300">
-          <thead className='bg-gray-50'>
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation Style</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Copy</th>
-            </tr>
-            
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            <CitationList referenceId={referenceId} citations={citations} setCitations={setCitations}/>
-          </tbody>
+        <div className='px-20'>
+          <CitationChoice referenceId={referenceId} citations={citations} setCitations={setCitations} />
+          <table className="min-w-full divide-y divide-gray-200 border border-gray-300">
+            <thead className='bg-gray-50'>
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation Style</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Citation</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Copy</th>
+              </tr>
+              
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <CitationList referenceId={referenceId} citations={citations} setCitations={setCitations}/>
+            </tbody>
 
-        </table>
+          </table>
+        </div>
       </div>
     </div>
+    
   )
 };

c66168140a0c5efa2ca98e5202c37932de6ef2b6
Muhammad Hamza Faisal <muhammadhamzafaisal@w-a8-8f-d9-4c-81-80.valpo.edu>
2024-04-03 13:39:43 -0500

    fixed up the footer's styling

--- cash-money-citations/src/app/layout.tsx
+++ cash-money-citations/src/app/layout.tsx
@@ -1,43 +1,49 @@
 import React, { Suspense } from "react";
-import { Metadata } from 'next';
-import NavBar from '../components/NavBar';
-import { AuthProvider } from "./Providers"
+import { Metadata } from "next";
+import NavBar from "../components/NavBar";
+import { AuthProvider } from "./Providers";
 import SideBar from "@/components/SideBar";
 import IconSidebar from "@/components/sidebar-components/IconSidebar";
 
 // Manages <head> HTML elements for built in SEO support
 export const metadata: Metadata = {
-    title: 'Home',
-    description: 'CashMoneyCitations'
-}
+  title: "Home",
+  description: "CashMoneyCitations",
+};
 
 export default async function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" data-theme="light">
-            <body>
-                <NavBar />
-                <SideBar />
-                <div className="content">
-                    <AuthProvider>{children}</AuthProvider>
-                </div>
+  return (
+    <html lang="en" data-theme="light">
+      <body>
+        <NavBar />
+        <SideBar />
+        <div className="content">
+          <AuthProvider>{children}</AuthProvider>
+        </div>
 
-                {/* <IconSidebar/> */}
+        {/* <IconSidebar/> */}
 
-                <footer className="fixed left-32 bottom-0 right-0 w-screen px-20 py-4 text-center text-sm text-gray-500 bg-gray-200">
-                    <p>
-                        Citation Styles created with help from the 
-                        <a href="https://citationstyles.org/" 
-                        className="text-blue-500 hover:underline hover:text-blue-700"> CSL (citation style language) project</a>
-                    </p>
-                    <p>
-                        © {new Date().getFullYear()} Cash Money Citations. All rights reserved.
-                    </p>
-                </footer>
-            </body>
-        </html>
-    )
+        <footer className="w-full px-4 py-4 text-center text-sm text-gray-500 bg-gray-200 sm:px-6 md:px-20">
+          <p>
+            Citation Styles created with help from the
+            <a
+              href="https://citationstyles.org/"
+              className="text-blue-500 hover:underline hover:text-blue-700"
+            >
+              {" "}
+              CSL (citation style language) project
+            </a>
+          </p>
+          <p>
+            © {new Date().getFullYear()} Cash Money Citations. All rights
+            reserved.
+          </p>
+        </footer>
+      </body>
+    </html>
+  );
 }

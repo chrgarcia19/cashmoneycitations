4801138fcd51a08e6ee5ef13e2eff104d5bf9cc6
xyclose11 <harrisgus1@hotmail.com>
2024-04-04 17:07:01 -0500

    Created a function that will convert the BibTex from the user into CSLJSON

--- cash-money-citations/src/app/input-bibtex/components/BibFileUpload.ts
+++ cash-money-citations/src/app/input-bibtex/components/BibFileUpload.ts
@@ -1,12 +1,13 @@
 'use server';
 
-// Take in user input files
-// Parse the bibtex
+// Take in user input files | Single file complete
+// Parse the bibtex | Parse single file complete
 // Validate the bibtex
 // Return to Textarea
 // Allow user to edit BibTex
 // Once user clicks submit, save to db as a reference and pass to CSL
-
+import { toCslJson } from "@/components/componentActions/citationActions";
+import { GiConsoleController } from "react-icons/gi";
 
 export const ParseBibTexUpload = async (formData: FormData) => {
     try {
@@ -19,5 +20,13 @@ export const ParseBibTexUpload = async (formData: FormData) => {
         return parsedBibFile;
     } catch(error) {
         return "Parsing BibTex File Failed."
+    }
+}
+
+export const SaveBibFileToDb = async (bibFile: string) => {
+    try {
+        const cslBibData = await toCslJson(bibFile);
+    } catch(e) {
+        console.error(e)
     }
 }

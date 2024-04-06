64ede01350c78e582fc6f4507c5fbe2585946599
thqmos <thomashohnholt@gmail.com>
2024-04-06 13:21:26 -0500

    Added ability to view contributors and indication of what references the user has owned

--- cash-money-citations/src/app/search/SearchField.tsx
+++ cash-money-citations/src/app/search/SearchField.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import searchRefs from "./searchRefs";
 import { useSession } from "next-auth/react";
 import { CreateCslJsonDocument, HandleManualReference } from "@/components/componentActions/citationActions";
+import { getUserReferences } from '../../components/componentActions/actions';
 import { useRouter } from "next/navigation";
 
 class SearchFieldProps {
@@ -14,6 +15,7 @@ const SearchField: React.FC<SearchFieldProps> = ({searchRefs}) => {
   const [refsArr, setRefsArr] = useState<any[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [tableShown, setTableShown] = useState<boolean>(false);
+  const [userOwnedRefs, setUserOwnedRefs] = useState<any[]>([]);
   const router = useRouter();
   
   async function getReference() {
@@ -21,20 +23,24 @@ const SearchField: React.FC<SearchFieldProps> = ({searchRefs}) => {
     let arr = await searchRefs(searchTerm);
     let arrJSON = JSON.parse(arr);
     setRefsArr(arrJSON);
+    //Checking to see if user owns refs or not
+    const userOwnedRefsData = await getUserReferences(session?.user?.id ?? '');
+    setUserOwnedRefs(userOwnedRefsData ?? []);
     setTableShown(true);
   }
 
   const addToDB = async (item: any) => {
     // Ensure item includes an ID field
     const itemWithId = { ...item, _id: undefined }; // Set _id to undefined to let MongoDB generate a new ID
-    const itemWithoutTags = { ...itemWithId, tags: []};
+    //Handling issues with tags
+    const itemWithoutTags = { ...itemWithId, tags: [] };
     HandleManualReference(itemWithoutTags, session?.user?.id)
     router.push("/reference-table");
     router.refresh();
   }
 
   return (
-    <div>
+    <div className="flex flex-col items-center h-screen">
       <div className="flex flex-col max-w-md mx-auto w-96">
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
@@ -48,32 +54,44 @@ const SearchField: React.FC<SearchFieldProps> = ({searchRefs}) => {
         </div>
       </div>
 
-      <div className="m-10 items-center w-auto">
-        {tableShown && (
-          <table className="table-auto mt-4 border-solid">
-            <thead className="bg-zinc-700 text-white">
-              <tr>
-                <th>Title</th>
-                <th>Date Published</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {refsArr.map(item => (
+      {tableShown && (
+        <table className="table-auto w-full mt-4 border-solid">
+          <thead className="bg-zinc-700 text-white">
+            <tr>
+              <th className="w-1/4 border-r">Title</th>
+              <th className="w-1/4 border-r">Date Published</th>
+              <th className="w-1/4 border-r">Contributors</th>
+              <th className="w-1/4 border-r">Your Reference?</th>
+              <th className="w-1/6">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {refsArr.map(item => {
+              // Check if the user owns the reference
+              const isUserOwned = userOwnedRefs.some(ref => ref._id === item._id);
+              return (
                 <tr key={item._id} className="border-b hover:bg-gray-100">
                   <td className="border-r border-b border-l border-zinc-500 py-2 px-2">{item.title[0]}</td>
                   <td className="border-r border-b border-l border-zinc-500 py-2 px-2">{item.date}</td>
                   <td className="border-r border-b border-l border-zinc-500 py-2 px-2">
+                    {item.contributors.map((contributor: any) => {
+                      return (
+                        <div key={contributor._id}>{contributor.firstName} {contributor.middleName} {contributor.lastName} {contributor.suffix}</div>
+                      );
+                    })}
+                  </td>
+                  <td className="border-r border-b border-l border-zinc-500 py-2 px-2">{isUserOwned ? "Yes" : "No"}</td>
+                  <td className="border-r border-b border-l border-zinc-500 py-2 px-2">
                     <button className="text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg w-24" onClick={() => addToDB(item)}>Add to my list</button>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
-  );
+  )
 };
 
 export default SearchField;

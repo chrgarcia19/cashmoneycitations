9d40b473afe08d5cc8899cba2de590d8c726c5fc
xyclose11 <harrisgus1@hotmail.com>
2024-04-07 16:54:30 -0500

    Update HandleInitialFormat function to include HandleContributors in editReferenceActions.ts

--- cash-money-citations/src/components/componentActions/editReferenceActions.ts
+++ cash-money-citations/src/components/componentActions/editReferenceActions.ts
@@ -1,13 +1,18 @@
 'use server'
 import dbConnect from "@/utils/dbConnect";
 import CSLBibModel from "@/models/CSLBibTex"
-import { HandleInitialFormat } from "@/components/componentActions/citationActions"
+import { HandleInitialFormat, HandleContributors } from "@/components/componentActions/citationActions"
 
 export async function EditReference(form: any, id: any) {
     await dbConnect();
     try {
         // Add ref id to form
         form.id = id;
+
+        // Ensures that unique contributor fields will be updated
+        await HandleContributors(form);
+
+        // Essentially puts it throught the same process the initial form takes when being made
         await HandleInitialFormat(form);
         await CSLBibModel.findByIdAndUpdate(id, form, {
         new: true,

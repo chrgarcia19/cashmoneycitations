8c779eafea07c7f44fe1c2b42f8910b44693506b
xyclose11 <harrisgus1@hotmail.com>
2024-04-15 16:45:58 -0500

    Added new fields

--- cash-money-citations/src/app/reference-table/components/data.ts
+++ cash-money-citations/src/app/reference-table/components/data.ts
@@ -1,11 +1,10 @@
 const columns = [
-  {name: "ID", uid: "id", sortable: true},
   {name: "TITLE", uid: "title", sortable: true},
   {name: "DATE PUBLISHED", uid: "datePublished", sortable: true},
   {name: "CONTRIBUTORS", uid: "contributors", sortable: true},
   {name: "TAGS", uid: "tags", sortable: true},
-  {name: "STATUS", uid: "status", sortable: true},
-  {name: "ACTIONS", uid: "actions"}
+  {name: "ACTIONS", uid: "actions"},
+  {name: "TYPE", uid: "type", sortable: true},
 ];
 
 const statusOptions = [

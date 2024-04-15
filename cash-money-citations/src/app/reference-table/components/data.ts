7758111ceee71860ff74ea92f94697a74c6a3561
xyclose11 <harrisgus1@hotmail.com>
2024-04-15 15:50:42 -0500

    Refactor column names in reference table component

--- cash-money-citations/src/app/reference-table/components/data.ts
+++ cash-money-citations/src/app/reference-table/components/data.ts
@@ -1,13 +1,9 @@
-import React from "react";
-import { getUserReferences } from "@/components/componentActions/actions";
-import { useSession } from "next-auth/react";
-
-
 const columns = [
   {name: "ID", uid: "id", sortable: true},
   {name: "TITLE", uid: "title", sortable: true},
-  {name: "Date Published", uid: "datePublished", sortable: true},
-  {name: "Contributors", uid: "contributors", sortable: true},
+  {name: "DATE PUBLISHED", uid: "datePublished", sortable: true},
+  {name: "CONTRIBUTORS", uid: "contributors", sortable: true},
+  {name: "TAGS", uid: "tags", sortable: true},
   {name: "STATUS", uid: "status", sortable: true},
 ];
 

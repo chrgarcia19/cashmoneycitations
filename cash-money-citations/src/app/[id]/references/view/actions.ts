269c1f6aada41ff690f964306446704f8efa7a40
xyclose11 <harrisgus1@hotmail.com>
2024-04-18 16:34:31 -0500

    Refactored CreateCitation function to support multiple referenceIds

--- cash-money-citations/src/app/[id]/references/view/actions.ts
+++ cash-money-citations/src/app/[id]/references/view/actions.ts
@@ -15,60 +15,108 @@ import CitationModel from "@/models/Citation";
 export async function CreateCitation(referenceId: any, styleChoice: string, localeChoice: string) {
 
     await dbConnect();
-    let referenceCslJson = await CSLBibModel.findById(referenceId)
-    const cslJson = referenceCslJson.cslJson
-    const referenceTitle = referenceCslJson.title
 
-    // Create a Cite instance with the references' cslJson data
-    const citation = new Cite(cslJson);
+    try {
+        let allCitations = [];
 
-    // Create a custom template and style for each specified style/locale
-    const templateName = styleChoice;
-    const localeName = localeChoice;
+        for (let id of referenceId) {
+            let referenceCslJson = await CSLBibModel.findById(id)
+            const cslJson = referenceCslJson.cslJson
+            await SaveCitationsToDB(id, referenceCslJson, styleChoice, localeChoice);
+            // Add the citation to allCitations
+            allCitations.push(cslJson);
+        }
 
-    // Find citation style where the name = the selected list
-    const styleData = await CSLStyleModel.findOne({
-        name: templateName,
-    }).exec()
-
-    // Find locale where name = inputted locale
-    const localeData = await CSLLocaleModel.findOne({
-        name: localeName,
-    }).exec()
-
-    const config = plugins.config.get('@csl')
+        // Create a Cite instance with the references' cslJson data
+        const citation = new Cite(allCitations);
     
-    // Add citation style & locale to Citation.js config
-    config.templates.add(templateName, styleData?.cslData);
-    config.locales.add(localeName, localeData?.localeData);
-
-    // Create custom citation with user specified style & locale
-    const customCitation = citation.format('bibliography', {
-        format: 'text',
-        template: templateName,
-        lang: localeName,
-    });
-
-    let newCustomCitation = await CitationModel.create({
-        name: templateName + referenceTitle,
-        style: styleData.title,
-        CitationData: customCitation,
-        language: localeName,
-    });
-
-    // Create/Update citation ID list
-    const citationIdList = referenceCslJson.citationIdList || [];
-    citationIdList.push(newCustomCitation.id);
-    await CSLBibModel.findByIdAndUpdate(referenceId, {
-        citationIdList: citationIdList
-    });
-
-    newCustomCitation = newCustomCitation.toObject();
-    newCustomCitation._id = newCustomCitation._id.toString();
+        // Create a custom template and style for each specified style/locale
+        const templateName = styleChoice;
+        const localeName = localeChoice;
     
-    return newCustomCitation;
+        // Find citation style where the name = the selected list
+        const styleData = await CSLStyleModel.findOne({
+            name: templateName,
+        }).exec()
+    
+        // Find locale where name = inputted locale
+        const localeData = await CSLLocaleModel.findOne({
+            name: localeName,
+        }).exec()
+    
+        const config = plugins.config.get('@csl')
+        
+        // Add citation style & locale to Citation.js config
+        config.templates.add(templateName, styleData?.cslData);
+        config.locales.add(localeName, localeData?.localeData);
+    
+        // Create custom citation with user specified style & locale
+        const customCitation = citation.format('bibliography', {
+            format: 'text',
+            template: templateName,
+            lang: localeName,
+        });
+
+        return customCitation;
+    } catch(e) {
+        console.error(e);
+    }
 }
 
+async function SaveCitationsToDB(refId: string, referenceCslJson: any, styleChoice: string, localeChoice: string) {
+
+        const cslJson = referenceCslJson.cslJson
+        const referenceTitle = referenceCslJson.title
+
+        // Create a Cite instance with the references' cslJson data
+        const citation = new Cite(cslJson);
+    
+        // Create a custom template and style for each specified style/locale
+        const templateName = styleChoice;
+        const localeName = localeChoice;
+    
+        // Find citation style where the name = the selected list
+        const styleData = await CSLStyleModel.findOne({
+            name: templateName,
+        }).exec()
+    
+        // Find locale where name = inputted locale
+        const localeData = await CSLLocaleModel.findOne({
+            name: localeName,
+        }).exec()
+    
+        const config = plugins.config.get('@csl')
+        
+        // Add citation style & locale to Citation.js config
+        config.templates.add(templateName, styleData?.cslData);
+        config.locales.add(localeName, localeData?.localeData);
+    
+        // Create custom citation with user specified style & locale
+        const customCitation = citation.format('bibliography', {
+            format: 'text',
+            template: templateName,
+            lang: localeName,
+        });
+    
+        let newCustomCitation = await CitationModel.create({
+            name: templateName + referenceTitle,
+            style: styleData.title,
+            CitationData: customCitation,
+            language: localeName,
+        });
+    
+        // Create/Update citation ID list
+        const citationIdList = referenceCslJson.citationIdList || [];
+        citationIdList.push(newCustomCitation.id);
+        await CSLBibModel.findByIdAndUpdate(refId, {
+            citationIdList: citationIdList
+        });
+    
+        newCustomCitation = newCustomCitation.toObject();
+        newCustomCitation._id = newCustomCitation._id.toString();
+        
+    
+}
 export async function GetBibLaTexFile(referenceId: string) {
     let referenceData = await CSLBibModel.findById(referenceId)
 

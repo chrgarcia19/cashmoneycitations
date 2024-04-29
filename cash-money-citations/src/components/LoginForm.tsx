548ef56f6522fae726c4a58ce52e31bb49c03996
chrgarcia19 <christian.garcia@valpo.edu>
2024-04-29 13:51:01 -0500

    Changed the props passed to the form field on the login page

--- cash-money-citations/src/components/LoginForm.tsx
+++ cash-money-citations/src/components/LoginForm.tsx
@@ -106,7 +106,7 @@ const LoginForm = ({formId, loginForm}: Props) => {
                         fieldName={'username'} 
                         fieldValue={username} 
                         fieldType={'text'} 
-                        fieldPlaceholder={'Enter your username'} 
+                        fieldPlaceholder={'Username'} 
                         handleChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => setUsername(e.target.value)} />
                 </div>
                 <div className='flex flex-col items-center'>
@@ -114,7 +114,7 @@ const LoginForm = ({formId, loginForm}: Props) => {
                         labelText={'Password'} 
                         fieldName={'password'} 
                         fieldValue={password} 
-                        fieldPlaceholder={"Enter your password"} 
+                        fieldPlaceholder={"Password"} 
                         handleChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => setPassword(e.target.value)} />
                 </div>
                 <Button

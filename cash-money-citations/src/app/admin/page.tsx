c3f141281698d11245dfdfacacf65581fec5679c
xyclose11 <harrisgus1@hotmail.com>
2024-04-30 16:58:22 -0500

    Updated styling of main admin page

--- cash-money-citations/src/app/admin/page.tsx
+++ cash-money-citations/src/app/admin/page.tsx
@@ -166,39 +166,7 @@ export const HeroSectionAdminHeader = () => {
         </svg>
       </div>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
-          <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-            The quick, brown fox
-            <br className="hidden md:block" />
-            jumps over a{' '}
-            <span className="relative inline-block px-2">
-              <div className="absolute inset-0 transform -skew-x-12 bg-teal-accent-400" />
-              <span className="relative text-teal-900">lazy dog</span>
-            </span>
-          </h2>
-          <p className="mb-6 text-base text-indigo-100 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae. explicabo. Sed ut perspiciatis unde omnis.
-          </p>
-          <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-16">
-            <input
-              placeholder="Email"
-              required
-              type="text"
-              className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-deep-purple-900 focus:border-teal-accent-700 focus:outline-none focus:shadow-outline"
-            />
-            <a
-              href="/"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md md:w-auto hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-teal-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Subscribe
-            </a>
-          </form>
-          <p className="max-w-md mb-10 text-xs tracking-wide text-indigo-100 sm:text-sm sm:mx-auto md:mb-16">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque.
-          </p>
+
           <DisplayServerStatistics />
           <DisplayCollectionStatistics />
           <h2 className='font-bold text-3xl p-2'>Logs</h2>
@@ -220,7 +188,6 @@ export const HeroSectionAdminHeader = () => {
           </a>
         </div>
       </div>
-    </div>
   );
 };
 

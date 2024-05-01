81a45d330bc9808a39c3618b8ecd7eff0fba2bca
chrgarcia19 <christian.garcia@valpo.edu>
2024-05-01 09:25:52 -0500

    Added the data-theme back to the html block and changed the description text in the password form field

--- cash-money-citations/src/app/layout.tsx
+++ cash-money-citations/src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className="flex flex-col justify-between min-h-screen bg-gray-100 dark:bg-gray-800">
         
         <Providers>

8c85724f4b2cacb2f567f863a88f918b095b316f
chrgarcia19 <christian.garcia@valpo.edu>
2024-05-08 11:45:32 -0500

    Finished modifying the dashboard

--- src/components/dashboardComponents/Dashboard.tsx
+++ src/components/dashboardComponents/Dashboard.tsx
@@ -34,7 +34,7 @@ const Dashboard = async () => {
     <div className="min-h-screen bg-gray-100 dark:bg-gray-800 py-10 px-5 md:px-10">
     <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-gray-200 mb-10">Welcome to Your Dashboard</h1>
     <div className="my-10">
-    <UserInfo />
+    <UserInfo user={user} />
     </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <AddReferences />
--- src/components/dashboardComponents/UserInfo.tsx
+++ src/components/dashboardComponents/UserInfo.tsx
@@ -3,55 +3,93 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import { Avatar, Button } from "@nextui-org/react";
 
-const UserInfo: React.FC = () => {
-  const [userInfo, setUserInfo] = useState({
-    name: "",
-    email: "",
-    profileImage: "/pfp.jpeg", // Default profile image
-    subscriptionType: "Premium", // Dummy data for subscription type
-    engagementLevel: "High", // Dummy data for engagement level
-  });
-  const [loading, setLoading] = useState(true);
+type Props = {
+  user: any;
+}
+
+const UserInfo = (props: Props) => {
 
   const { data: session, status } = useSession();
+
+  const [userInfo, setUserInfo] = useState({
+    name: props.user.firstName + " " + props.user.lastName,
+    username: props.user.username,
+    email: props.user.email,
+    role: props.user.role,
+  });
 
   useEffect(() => {
     if (status === "authenticated" && session?.user) {
       setUserInfo({
-        name: session.user.name || "Anonymous",
-        email: session.user.email || "No email available",
-        profileImage: session.user.image || "/pfp.jpeg", // Check for user image, or use default
-        subscriptionType: "Premium", // Dummy data
-        engagementLevel: "High", // Dummy data
+        name: props.user.firstName + " " + props.user.lastName,
+        username: props.user.username,
+        email: props.user.email,
+        role: props.user.role,
       });
-      setLoading(false);
     }
   }, [session, status]);
 
   return (
     <div className="bg-white dark:bg-gray-700 rounded-lg shadow p-6 flex flex-col items-center justify-between transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-green-800 hover:shadow-lg">
-      <h2 className="text-xl font-semibold mb-2 text-gray-800 dark:text-gray-200">User Information</h2>
+      <h2 className="text-2xl underline font-semibold mb-2 text-gray-800 dark:text-gray-200">User Information</h2>
       <div className="flex flex-row items-center">
         <div className="flex-shrink-0">
-          <img src={userInfo.profileImage} alt="User Profile" className="w-32 h-32 rounded-full mb-4 object-cover" />
-        </div>
-        <div className="ml-6 text-center">
-          {loading ? (
-            <p className="text-gray-600 dark:text-gray-400">Loading...</p>
-          ) : (
-            <>
-              <p className="text-lg font-bold text-gray-800 dark:text-gray-200">Name: {userInfo.name}</p>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Email: {userInfo.email}</p>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Subscription: {userInfo.subscriptionType}</p>
-              <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">Engagement Level: {userInfo.engagementLevel}</p>
-              <Link href="/profile">
-                <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
-                  Go to Profile
-                </button>
-              </Link>
-            </>
+          {session?.user?.role === "admin" && (
+            <Avatar
+              isBordered
+              className="transition-transform w-32 h-32 rounded-full mb-4 object-cover"
+              color="danger"
+              size="lg"
+              src={session?.user?.image ?? undefined}
+              showFallback
+            />
+          ) || session?.user?.role === "user" && (
+            <Avatar
+              isBordered
+              className="transition-transform w-32 h-32 rounded-full mb-4 object-cover"
+              color="success"
+              size="lg"
+              src={session?.user?.image ?? undefined}
+              showFallback
+            />
+          ) || (
+            <Avatar
+              isBordered
+              className="transition-transform w-32 h-32 rounded-full mb-4 object-cover"
+              color="default"
+              size="lg"
+              src={session?.user?.image ?? undefined}
+              showFallback
+            />
           )}
+          
+        </div>
+        <div className="ml-6 text-left">
+          <>
+            {userInfo.name && (
+              <p className="text-lg font-bold text-gray-800 dark:text-white">Name: {userInfo.name}</p>
+            ) || (
+              <p className="text-lg font-bold text-gray-800 dark:text-white">Username: {userInfo.username}</p>
+            )}
+            {userInfo.username && (
+              <p className="text-sm text-black dark:text-white">Username: {userInfo.username}</p>
+            )}
+            {userInfo.email && (
+              <p className="text-sm text-black dark:text-white">Email: {userInfo.email}</p>
+            )}
+            {userInfo.role && (
+              <p className="text-sm text-black dark:text-white">Role: {userInfo.role}</p>
+            )}
+            <Button className="mt-5 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+              color="primary"
+              size="lg"
+              as={Link}
+              href={`/profile/${session?.user?.id}?id=${session?.user?.id}`}>
+                  User Profile
+            </Button>
+          </>
         </div>
       </div>
     </div>
